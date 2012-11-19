78317536dab1b1e46ce909a159ee3de2cdf1fbab
Luigi Marini <lmarini@illinois.edu>
2012-11-19 15:41:33 -0700

    Moved users actions to standalone Users controller.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -75,43 +75,6 @@ object Application extends Controller {
   def testJson = Action {
     Ok("{test:1}").as(JSON)
   }
-  
-  /**
-   * List users.
-   */
-  def list() = Action {
-    val users = User.findAll
-    Ok(views.html.list(users))
-  }
-
-  /**
-   * List users by country.
-   */
-  def listByCountry(country: String) = Action {
-    val users = User.findByCountry(country)
-    Ok(views.html.list(users))
-  }
-
-  /**
-   * View user.
-   */
-  def view(id: ObjectId) = Action {
-    User.findOneById(id).map( user =>
-      Ok(views.html.user(user))
-    ).getOrElse(NotFound)
-  }
-
-  /**
-   * Create new user.
-   */
-  def create(username: String) = Action {
-    val user = User(
-      username = username,
-      password = "1234"
-    )
-    User.save(user)
-    Ok(views.html.user(user))
-  }
  
   /**
    * Upload form.
--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,11 +1,49 @@
 package controllers
-import play.api.mvc.Controller
+import play.api.mvc._
+import models.User
+import org.bson.types.ObjectId
 
 /**
  * Manage users.
  * 
  * @author Luigi Marini
  */
-class Users extends Controller {
+object Users extends Controller {
+  
+  /**
+   * List users.
+   */
+  def list() = Action {
+    val users = User.findAll
+    Ok(views.html.list(users))
+  }
 
+  /**
+   * List users by country.
+   */
+  def listByCountry(country: String) = Action {
+    val users = User.findByCountry(country)
+    Ok(views.html.list(users))
+  }
+
+  /**
+   * View user.
+   */
+  def view(id: ObjectId) = Action {
+    User.findOneById(id).map( user =>
+      Ok(views.html.user(user))
+    ).getOrElse(NotFound)
+  }
+
+  /**
+   * Create new user.
+   */
+  def create(username: String) = Action {
+    val user = User(
+      username = username,
+      password = "1234"
+    )
+    User.save(user)
+    Ok(views.html.user(user))
+  }
 }
