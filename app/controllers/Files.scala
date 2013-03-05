9209c355ad0b36ae7e9493fba168c5b859466559
Luigi Marini <lmarini@illinois.edu>
2013-03-04 18:44:08 -0600

    Fixed bad merge.

--- app/controllers/Files.scala
+++ app/controllers/Files.scala
@@ -162,7 +162,7 @@ object Files extends Controller with securesocial.core.SecureSocial {
             // TODO RK : need figure out if we can use https
             val host = "http://" + request.host + request.path.replaceAll("upload$", "")
             val id = f.id.toString
-            current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, host, key))}
+            current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, host, key, Map.empty))}
             current.plugin[ElasticsearchPlugin].foreach{
               _.index("files", "file", id, List(("filename",f.filename), ("contentType", f.contentType)))
             }
