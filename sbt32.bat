2b7065fb28d62ca8bb64f5fdaeed3161eaba87c0
Ashwini Vaidya <vaidya10@illinois.edu>
2013-05-24 11:06:58 -0500

    Rob modified for 32 and 64 bit computers

--- sbt32.bat
+++ sbt32.bat
@@ -1,3 +1,3 @@
 set SCRIPT_DIR=%~dp0
 
-java -Xms1024M -Xmx1024M -Xss1M -XX:+CMSClassUnloadingEnabled -jar "%SCRIPT_DIR%\sbt-launch.jar" %*
+java -Xms1024M -Xmx1024M -Xss1M -XX:MaxPermSize=128M -XX:+CMSClassUnloadingEnabled -jar "%SCRIPT_DIR%\sbt-launch.jar" %*
