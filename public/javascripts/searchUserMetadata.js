cb40cfed1b45d8eba47f48359cd986c8a63fba49
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-06-21 19:39:22 +0300

    Removed a debug log.

--- public/javascripts/searchUserMetadata.js
+++ public/javascripts/searchUserMetadata.js
@@ -270,7 +270,6 @@ $(function() {
 				  else if($(this).html() == "Submit"){
 
 					var data = DOMtoJSON(document.getElementById('datasetUserMetadata').children[1]);
-					alert(JSON.stringify(data));
 					var request = $.ajax({
 				       type: 'POST',
 				       url: queryIp,
