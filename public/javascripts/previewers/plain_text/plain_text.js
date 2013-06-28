51ffcb7e2397bc3c8b78489c4551d1faefa60d56
Luigi Marini <lmarini@illinois.edu>
2013-06-28 16:08:43 -0500

    Simple text previewer. Loads everything for now. Need to put a limit on the number of characters.

---
+++ public/javascripts/previewers/plain_text/plain_text.js
@@ -0,0 +1,53 @@
+(function ($, Configuration) {
+  console.log("Plain text configuration " + Configuration);
+
+  var fileUrl = "http://" + Configuration.hostIp + ":" + window.location.port + Configuration.url;
+  var pathJs = "http://" + Configuration.hostIp + ":" + window.location.port + Configuration.jsPath + "/";
+  
+  var width = 750;
+  var height = 550;
+    
+  $(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: fileUrl,
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+  });
+
+  function processData(allText) {
+
+	var allTextLines = allText.split(/\r\n|\n/);
+
+	var table = "<div id=\"table-scroll\"><table width=\"700\" height=\"550\" border=\"0\">";
+
+
+	for (var i=0; i<allTextLines.length; i++) {
+		var line = allTextLines[i].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+		console.log(line);
+		table += "<tr><td>"+line+"</td></tr>";
+	}
+
+	table += "</table></div>";
+
+	$(Configuration.tab).append(table);
+
+    // var record_num = 5;  // or however many elements there are in each row
+    // var allTextLines = allText.split(/\r\n|\n/);
+    // var entries = allTextLines[0].split(',');
+    // var lines = [];
+
+    // var headings = entries.splice(0,record_num);
+    // while (entries.length>0) {
+    //     var tarr = [];
+    //     for (var j=0; j<record_num; j++) {
+    //         tarr.push(headings[j]+":"+entries.shift());
+    //     }
+    //     lines.push(tarr);
+    // }
+    // alert(lines);
+  }
+
+  
+}(jQuery, Configuration));
