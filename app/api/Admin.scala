b5195c5267669d7654495843f978767cc565d129
Luigi Marini <lmarini@illinois.edu>
2013-07-15 12:37:29 -0500

    Admin endpoint to drop all data in the system. Keeps users and other tables.

---
+++ app/api/Admin.scala
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package api
+
+import play.api.mvc.Controller
+import play.api.mvc.Action
+import play.api.Play.current
+import services.MongoSalatPlugin
+import play.api.Logger
+import play.api.libs.json.Json.toJson
+
+/**
+ * Admin endpoints for JSON API.
+ * 
+ * 
+ * @author Luigi Marini
+ *
+ */
+object Admin extends Controller with ApiController {
+
+  /**
+   * DANGER
+   */
+  def deleteAllData = Authenticated {
+    Action {
+      current.plugin[MongoSalatPlugin].map { mongo =>
+	      mongo.sources.values.map { source =>
+	        Logger.debug("**DANGER** Deleting data collections **DANGER**")
+	        source.collection("datasets").drop()
+	        source.collection("previews.chunks").drop()
+	        source.collection("previews.files").drop()
+	        source.collection("sections").drop()
+	        source.collection("uploads.chunks").drop()
+	        source.collection("uploads.files").drop()
+	        source.collection("uploadquery").drop()
+	        source.collection("extractions").drop()
+	        source.collection("streams").drop()
+	        Logger.debug("**DANGER** Data deleted **DANGER**")
+	      }
+      }
+      Ok(toJson("done"))
+    }
+  }
+}
