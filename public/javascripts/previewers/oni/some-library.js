551555845b0066cb0b7dc762285fbb59c1ee4343
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-09-30 12:39:30 +0300

    Bugfixes for oni previewer and also adaption of it to dataset and file pages with possibly many different oni previews.

--- public/javascripts/previewers/oni/some-library.js
+++ public/javascripts/previewers/oni/some-library.js
@@ -1,6 +1,6 @@
 function updatex3dPosition(prNum){
 	if(window["oldx3dposition" + prNum] != window["thisPreview" + prNum].offset().top){
-		$("#x3dElement" + prNum).attr("style", "position:absolute;top:" + (window["thisPreview" + prNum].offset().top + 288) + "px;");
+		$("#x3dElement" + prNum).attr("style", "position:absolute;top:" + (window["thisPreview" + prNum].offset().top) + "px;");
 		window["oldx3dposition" + prNum] = window["thisPreview" + prNum].offset().top;
 	}
 }
@@ -9,12 +9,13 @@ function renderSingleFrame(prNum,index){
 	
 	var width = 750;
 	var height = 550;
-	var inner = "<x3d id='x3dElement" + prNum + "' showStat='false' showLog='false' height='" + height + "px' width='" + width + "px' x='0px' y='0px' style='position:absolute;top:" + ($(Configuration.tab).offset().top + 288) + "px;'>";
+	var inner = "<x3d id='x3dElement" + prNum + "' showStat='false' showLog='true' height='" + height + "px' width='" + width + "px' x='0px' y='0px' style='position:absolute;top:" + ($(Configuration.tab).offset().top) + "px;'>";
 	
 
-	if((bufferWriteCounter - bufferReadCounter) > 50)
+	//if((bufferWriteCounter - bufferReadCounter) > 50)
+	if((window["bufferWriteCounter" + prNum] - window["bufferReadCounter" + prNum]) > 0)	
 	{
-		if(frameArray[index] != undefined){
+		if(window["frameArray" + prNum][index] != undefined){
 			$("#x3dElement"+prNum).replaceWith(inner + frameArray[index] + "</x3d>");
 			x3dom.reload();
 			bufferReadCounter++;
@@ -22,27 +23,27 @@ function renderSingleFrame(prNum,index){
 			console.log("render frame " + index);
 		}
 		else{
-			setTimeout("renderSingleFrame('" + prNum + "'," + (index) + ");",30);
+			setTimeout("renderSingleFrame('" + prNum + "'," + (index+1) + ");",30);
 		}
 	}
 	else {
-		setTimeout("renderSingleFrame('" + prNum + "'," + (index) + ");",100);
+		//setTimeout("renderSingleFrame('" + prNum + "'," + (index) + ");",100);
 	}			
 
 }
 
 
-function fetchFrame(index){
+function fetchFrame(index,prNum){
 
 	$.ajax({
-		    url: previewUrl + previewFileIds[index],
+		    url: previewUrl + window["previewFileIds" + prNum][index],
 		    async:true,
 		    success: function (data,status,jqXHR) {
 				
-				if(index < previewFileIds.length-2) {
-					fetchFrame(index+1);
-					bufferWriteCounter++;
-					frameArray[index] = data;
+				if(index < window["previewFileIds" + prNum].length-1) {
+					fetchFrame(index+1, prNum);
+					window["bufferWriteCounter" + prNum]++;
+					window["frameArray" + prNum][index] = data;
 					console.log("fetched file: " + index);
 				}				
 		  	},
@@ -50,10 +51,10 @@ function fetchFrame(index){
 		    error: function (obj, status, error) {				
 				console.log("Error fetching preview file:"  + index + " " + status + " " + error);
 
-				if(index < previewFileIds.length-2) {
-					fetchFrame(index+1);
-					bufferWriteCounter++;
-					frameArray[index] = data;
+				if(index < window["previewFileIds" + prNum].length-1) {
+					fetchFrame(index+1, prNum);
+					window["bufferWriteCounter" + prNum]++;
+					window["frameArray" + prNum][index] = data;
 				}
 			}
 	});
@@ -72,25 +73,25 @@ function fetchFrame(index){
   var cacheLength = 5;  
   var prNum = Configuration.tab.replace("#previewer","");
 
- $(Configuration.tab).append("<table id='x3dElementTable" + prNum + "' style ='margin-bottom:560px;'></table>")
+ $(Configuration.tab).append("<table id='x3dElementTable" + prNum + "' style ='margin-bottom:560px;'></table>");
 
-  var inner = "<x3d id='x3dElement" + prNum + "' showStat='false' showLog='false' showProgress='false' height='" + height + "px' width='" + width + "px' x='0px' y='0px' style='position:absolute;top:" +    ($(Configuration.tab).offset().top ) + "px;'>";
+  var inner = "<x3d id='x3dElement" + prNum + "' showStat='false' showLog='true' showProgress='false' height='" + height + "px' width='" + width + "px' x='0px' y='0px' style='position:absolute;top:" +    ($(Configuration.tab).offset().top ) + "px;'>";
   window["oldx3dposition" + prNum] = $(Configuration.tab).offset().top;
   window["thisPreview" + prNum] = $(Configuration.tab);
  
-//  setInterval("updatex3dPosition('" + prNum + "');", 50);
+  setInterval("updatex3dPosition('" + prNum + "');", 50);
 
    var s = document.createElement("script");
   s.type = "text/javascript";
- // s.src = pathJs + "x3dom.js";
-  s.src = "http://www.x3dom.org/release/x3dom.js"
+  s.src = pathJs + "x3dom.js";
+  //s.src = "http://www.x3dom.org/release/x3dom.js";
   console.log("Updating tab " + Configuration.tab);
   $(Configuration.tab).append(s);  
 
-  previewFileIds = [];
-  bufferReadCounter = 0;
-  bufferWriteCounter = 0;
-  indexPreviewIdMap = [];
+  window["previewFileIds" + prNum] = [];
+  window["bufferReadCounter" + prNum] = 0;
+  window["bufferWriteCounter" + prNum] = 0;
+  window["indexPreviewIdMap" + prNum] = [];
 
   //fetching the list of preview files
 
@@ -98,8 +99,8 @@ function fetchFrame(index){
 	    url: fileUrl,
 	    async:false,
 	    success: function (data) {
-			previewFileIds = data.split(",");
-			frameArray = new Array(previewFileIds.length-1);			
+	    	window["previewFileIds" + prNum] = data.split(",");
+			window["frameArray" + prNum] = new Array(window["previewFileIds" + prNum].length-1);			
 	    	 },
 	    dataType: 'text',
 	    error: function (obj, status, error) {
@@ -109,13 +110,13 @@ function fetchFrame(index){
 
 
 	$.ajax({
-	    url: previewUrl + previewFileIds[15],
+	    url: previewUrl + window["previewFileIds" + prNum][0],
 	    async:false,
 	    success: function (data) {
 			$(Configuration.tab).append(inner + data + "</x3d>");
-			frameArray[0] = data;
-			bufferWriteCounter++;
-			bufferReadCounter++;
+			window["frameArray" + prNum][0] = data;
+			window["bufferWriteCounter" + prNum]++;
+			window["bufferReadCounter" + prNum]++;
 			
 	    	 },
 	    dataType: 'text',
@@ -124,7 +125,7 @@ function fetchFrame(index){
 		}
 	});
 	
-	//fetchFrame(1);
-	//setTimeout("renderSingleFrame('" + prNum + "'," + 1 + ");",1000);
+	fetchFrame(1,prNum);  
+	setTimeout("renderSingleFrame('" + prNum + "'," + 1 + ");",1000); 
 
 }(jQuery, Configuration));
