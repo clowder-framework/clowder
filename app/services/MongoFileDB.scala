9e26381cc3c969907faf651ed3fd258d1a0de0e7
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-10-10 16:27:30 +0300

    Extractor intermediate results files not presented to users.

--- app/services/MongoFileDB.scala
+++ app/services/MongoFileDB.scala
@@ -39,11 +39,11 @@ trait MongoFileDB {
   def listFilesAfter(date: String, limit: Int): List[File] = {
     val order = MongoDBObject("uploadDate"-> -1)
     if (date == "") {
-      FileDAO.findAll.sort(order).limit(limit).toList
+      FileDAO.find("isIntermediate" $ne true).sort(order).limit(limit).toList
     } else {
       val sinceDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(date)
       Logger.info("After " + sinceDate)
-      FileDAO.find("uploadDate" $lt sinceDate).sort(order).limit(limit).toList
+      FileDAO.find($and("isIntermediate" $ne true, "uploadDate" $lt sinceDate)).sort(order).limit(limit).toList
     }
   }
   
@@ -53,12 +53,12 @@ trait MongoFileDB {
   def listFilesBefore(date: String, limit: Int): List[File] = {
     var order = MongoDBObject("uploadDate"-> -1)
     if (date == "") {
-      FileDAO.findAll.sort(order).limit(limit).toList
+      FileDAO.find("isIntermediate" $ne true).sort(order).limit(limit).toList
     } else {
       order = MongoDBObject("uploadDate"-> 1) 
       val sinceDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(date)
       Logger.info("Before " + sinceDate)
-      var fileList = FileDAO.find("uploadDate" $gt sinceDate).sort(order).limit(limit + 1).toList.reverse
+      var fileList = FileDAO.find($and("isIntermediate" $ne true, "uploadDate" $gt sinceDate)).sort(order).limit(limit + 1).toList.reverse
       fileList = fileList.filter(_ != fileList.last)
       fileList      
     }
