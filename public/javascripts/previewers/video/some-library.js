68152e65551dc4b846003b5caf873498b286b999
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-10-25 11:54:11 +0300

    Video and QTVR previewers MIME types associations fixes.

--- public/javascripts/previewers/video/some-library.js
+++ public/javascripts/previewers/video/some-library.js
@@ -11,7 +11,4 @@
      "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
   );
   
-  $(Configuration.tab).append("<br/><p>Note: Quicktime VR panoramas (QTVR) cannot be viewed using this previewer. You can use the \"Quicktime\"" +
-								" previewer to interact with them (if on Windows or Mac) or download the QTVR and view it on your desktop.</p>");
-     
 }(jQuery, Configuration));
