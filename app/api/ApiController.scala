7c54bb9c4f58839b121da48d6f94ad024102676b
Luigi Marini <lmarini@illinois.edu>
2013-10-26 16:50:18 -0500

    Updated to bootstrap 3. Most styles updated to new ones.

--- app/api/ApiController.scala
+++ app/api/ApiController.scala
@@ -1,65 +1,3 @@
-<<<<<<< Upstream, based on play-2.2
-package api
-
-import play.api.Logger
-import play.api.mvc.Action
-import play.api.mvc.BodyParser
-import play.api.mvc.Controller
-import play.api.mvc.Result
-import securesocial.core.AuthenticationMethod
-import securesocial.core.Authorization
-import securesocial.core.SecureSocial
-import securesocial.core.SocialUser
-import securesocial.core.IdentityId
-
-/**
- * New way to wrap actions for authentication so that we have access to Identity.
- *
- * @author Rob Kooper
- *
- */
-trait ApiController extends Controller {
-  val anonymous = new SocialUser(new IdentityId("anonymous", ""), "Anonymous", "User", "Anonymous User", None, None, AuthenticationMethod.UserPassword)
-
-  def SecuredAction[A](p: BodyParser[A] = parse.json, authorization: Authorization = WithPermission(Permission.Public))(f: RequestWithUser[A] => Result) = Action(p) {
-    implicit request =>
-      {
-        request.queryString.get("key") match { // token in url
-          case Some(key) => {
-            if (key.length > 0) {
-              // TODO Check for key in database
-              if (key(0).equals(play.Play.application().configuration().getString("commKey"))) {
-                if (authorization.isAuthorized(anonymous))
-                  f(RequestWithUser(Some(anonymous), request))
-                else
-                  Unauthorized("Not authorized")
-              } else {
-                Logger.debug("Key doesn't match")
-                Unauthorized("Not authenticated")
-              }
-            } else
-              Unauthorized("Not authenticated")
-          }
-          case None => {
-            SecureSocial.currentUser(request) match { // calls from browser
-              case Some(identity) => {
-                if (authorization.isAuthorized(identity))
-                  f(RequestWithUser(Some(identity), request))
-                else
-                  Unauthorized("Not authorized")
-              }
-              case None => {
-                if (authorization.isAuthorized(null))
-                  f(RequestWithUser(None, request))
-                else
-                  Unauthorized("Not authorized")
-              }
-            }
-          }
-        }
-      }
-  }
-=======
 package api
 
 import play.api.Logger
@@ -120,5 +58,4 @@ trait ApiController extends Controller {
         }
       }
   }
->>>>>>> 8d0afe8 Upgraded to play 2.2. Compile errors are all fixed plus a few runtime errors. Current runtime error is:
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,3 @@
-<<<<<<< Upstream, based on play-2.2
 import sbt._
 import Keys._
 import play.Project._
@@ -11,24 +10,9 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "com.novus" %% "salat" % "1.9.2" exclude("org.scala-stm", "scala-stm_2.10.0"),
     "securesocial" %% "securesocial" % "master-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0"),
-=======
-import sbt._
-import Keys._
-import play.Project._
-
-object ApplicationBuild extends Build {
-
-  val appName = "medici-play"
-  val appVersion = "1.0-SNAPSHOT"
-
-  val appDependencies = Seq(
-    "com.novus" %% "salat" % "1.9.2" exclude("org.scala-stm", "scala-stm_2.10.0"),
-    "securesocial" %% "securesocial" % "master-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0"),
->>>>>>> 8d0afe8 Upgraded to play 2.2. Compile errors are all fixed plus a few runtime errors. Current runtime error is:
     "com.rabbitmq" % "amqp-client" % "3.0.0",
     "org.elasticsearch" % "elasticsearch" % "0.90.2",
     "com.spatial4j" % "spatial4j" % "0.3",
-<<<<<<< Upstream, based on play-2.2
     "org.mongodb" %% "casbah" % "2.6.3",
     "postgresql" % "postgresql" % "9.1-901.jdbc4",
     "com.wordnik" %% "swagger-play2" % "1.2.6-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0"),
@@ -52,27 +36,3 @@ object ApplicationBuild extends Build {
     resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
   ).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
 }
-=======
-    "org.mongodb" %% "casbah" % "2.6.2",
-    "postgresql" % "postgresql" % "9.1-901.jdbc4",
-    "com.wordnik" %% "swagger-play2" % "1.2.6-SNAPSHOT" exclude("org.scala-stm", "scala-stm_2.10.0"),
-    "org.reflections" % "reflections" % "0.9.9-RC1"
-  )
-
-  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
-  def customLessEntryPoints(base: File): PathFinder = (
-    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
-    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "responsive.less") +++
-    (base / "app" / "assets" / "stylesheets" * "*.less")
-  )
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    lessEntryPoints <<= baseDirectory(customLessEntryPoints),
-    routesImport += "Binders._",
-    templatesImport += "org.bson.types.ObjectId",
-    resolvers += Resolver.url("sbt-plugin-releases", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
-    resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
-    resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
-  ).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
-}
->>>>>>> 8d0afe8 Upgraded to play 2.2. Compile errors are all fixed plus a few runtime errors. Current runtime error is:
