18a8820051ad87e6cffaf4fdfde6d20858065bdc
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-10-29 13:22:16 +0200

    Autoplay set to False for Quicktime videos.

--- public/javascripts/previewers/Quicktime/some-library.js
+++ public/javascripts/previewers/Quicktime/some-library.js
@@ -11,10 +11,10 @@
   $(Configuration.tab).append(
 		  "<object classid='clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B' height='"+ height +"' width='"+ width +"'>"+
 		  "<param name='src' value='" + "http://"+Configuration.hostIp+":"+window.location.port+ Configuration.url   +"' />"+
-		  "<param name='autoplay' value='true'>"+
+		  "<param name='autoplay' value='false'>"+
 		  "<param name='controller' value='true'>"+
 		  "<param name='scale' value='tofit'>"+
-		  "<embed type='video/quicktime' alt='No video plugin capable of playing mov videos was found.' src='" + "http://"+Configuration.hostIp+":"+window.location.port+ Configuration.url   +"' height='"+ height +"' width='"+ width +"' autoplay='true' controller='true' scale='tofit'></embed>"+
+		  "<embed type='video/quicktime' alt='No video plugin capable of playing mov videos was found.' src='" + "http://"+Configuration.hostIp+":"+window.location.port+ Configuration.url   +"' height='"+ height +"' width='"+ width +"' autoplay='false' controller='true' scale='tofit'></embed>"+
 		  "</object>"		  
   );
 
