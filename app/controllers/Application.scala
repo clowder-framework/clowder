389635e9403d9bd78ac6f379fe5359790e13301f
Luigi Marini <lmarini@illinois.edu>
2013-11-05 09:27:28 -0600

    Main page carousel showing the latest five files. Commented out by default since it needs a few more checks.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,8 @@ import play.api.mvc.Controller
 import api.Sections
 import api.WithPermission
 import api.Permission
+import models.FileDAO
+import com.mongodb.casbah.commons.MongoDBObject
 
 /**
  * Main application controller.
@@ -22,7 +24,8 @@ object Application extends SecuredController {
 //  }
   def index = SecuredAction() { request =>
   	implicit val user = request.user
-    Ok(views.html.index())
+  	val latestFiles = FileDAO.find(MongoDBObject()).sort(MongoDBObject("uploadDate" -> -1)).limit(5).toList
+    Ok(views.html.index(latestFiles))
   }
   
   /**
