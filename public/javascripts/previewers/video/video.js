29ea1d93a2a8284ed2876fc4e8304018a87da89a
Luigi Marini <lmarini@illinois.edu>
2013-11-14 11:16:45 -0600

    Split basic video player and popcorn player into two separate previewers while we figure out MMDB-1377

--- public/javascripts/previewers/video/video.js
+++ public/javascripts/previewers/video/video.js
@@ -1,15 +1,5 @@
 (function ($, Configuration) {
-  console.log("video previewer for " + Configuration.id);
-  
-//  var s = document.createElement("script");
-//  s.type = "text/javascript";
-//  s.src = "http://popcornjs.org/code/dist/popcorn-complete.min.js";
-//    
-//  console.log("Updating tab " + Configuration.tab);
-//  $(Configuration.tab).append(s);  
-//  $(Configuration.tab).append(			  
-//     "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
-//  );
+  console.log("Starting video previewer for " + Configuration.id);
   
   var height = 400;  
   var width = 750;
@@ -21,6 +11,5 @@
   $(Configuration.tab).append(			  
      "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
   );
-  
-  
+
 }(jQuery, Configuration));
