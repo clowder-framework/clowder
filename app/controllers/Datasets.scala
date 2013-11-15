5c6588983aa5bb37144920412718c7f2eb7524d2
c.sophocleous <c.sophocleous@cyi.ac.cy>
2013-11-15 14:40:10 +0200

    Can create new dataset based on existing file instead of a new file.

--- app/controllers/Datasets.scala
+++ app/controllers/Datasets.scala
@@ -59,7 +59,8 @@ object Datasets extends SecuredController {
    
   def newDataset()  = SecuredAction(authorization=WithPermission(Permission.CreateDatasets)) { implicit request =>
     implicit val user = request.user
-  	Ok(views.html.newDataset(datasetForm)).flashing("error"->"Please select a file") 
+    val filesList = for(file <- Services.files.listFiles.sortBy(_.filename)) yield (file.id.toString(), file.filename)
+  	Ok(views.html.newDataset(datasetForm, filesList)).flashing("error"->"Please select ONE file (upload new or existing)") 
   }
    
   /**
@@ -212,103 +213,140 @@ object Datasets extends SecuredController {
     user match {
       case Some(identity) => {
         datasetForm.bindFromRequest.fold(
-          errors => BadRequest(views.html.newDataset(errors)),
+          errors => BadRequest(views.html.newDataset(errors, for(file <- Services.files.listFiles.sortBy(_.filename)) yield (file.id.toString(), file.filename))),
 	      dataset => {
 	           request.body.file("file").map { f =>
-	            var nameOfFile = f.filename
-	            var flags = ""
-	            if(nameOfFile.endsWith(".ptm")){
-	              var thirdSeparatorIndex = nameOfFile.indexOf("__")
-	              if(thirdSeparatorIndex >= 0){
-	                var firstSeparatorIndex = nameOfFile.indexOf("_")
-	                var secondSeparatorIndex = nameOfFile.indexOf("_", firstSeparatorIndex+1)
-	            	flags = flags + "+numberofIterations_" +  nameOfFile.substring(0,firstSeparatorIndex) + "+heightFactor_" + nameOfFile.substring(firstSeparatorIndex+1,secondSeparatorIndex)+ "+ptm3dDetail_" + nameOfFile.substring(secondSeparatorIndex+1,thirdSeparatorIndex)
-	            	nameOfFile = nameOfFile.substring(thirdSeparatorIndex+2)
-	              }
-	            }
-
-		        Logger.debug("Uploading file " + nameOfFile)
-		        
-		        // store file
-		        Logger.info("Adding file" + identity)
-		        val showPreviews = request.body.asFormUrlEncoded.get("datasetLevel").get(0)
-			    val file = Services.files.save(new FileInputStream(f.ref.file), nameOfFile, f.contentType, identity, showPreviews)
-			    Logger.debug("Uploaded file id is " + file.get.id)
-			    Logger.debug("Uploaded file type is " + f.contentType)
-			    
-			    val uploadedFile = f
-			    file match {
-			      case Some(f) => {
-			        current.plugin[FileDumpService].foreach{_.dump(DumpOfFile(uploadedFile.ref.file, f.id.toString, nameOfFile))}
-			        
-			        val id = f.id.toString	                	                
-	                if(showPreviews.equals("FileLevel"))
-	                	flags = flags + "+filelevelshowpreviews"
-	                else if(showPreviews.equals("None"))
-	                	flags = flags + "+nopreviews"
-			        var fileType = f.contentType
-			        if(fileType.contains("/zip") || fileType.contains("/x-zip") || nameOfFile.endsWith(".zip")){
-			          fileType = FilesUtils.getMainFileTypeOfZipFile(uploadedFile.ref.file, nameOfFile, "dataset")			          
-			          if(fileType.startsWith("ERROR: ")){
-			             Logger.error(fileType.substring(7))
-			             InternalServerError(fileType.substring(7))
-			          }			          
-			        }else if(nameOfFile.endsWith(".mov")){
-			        	fileType = "ambiguous/mov";
-			        }
-			        
-			        
-			    	// TODO RK need to replace unknown with the server name
-			    	val key = "unknown." + "file."+ fileType.replace(".", "_").replace("/", ".")
-//			        val key = "unknown." + "file."+ "application.x-ptm"
-			    	
-	                // TODO RK : need figure out if we can use https
-	                val host = "http://" + request.host + request.path.replaceAll("dataset/submit$", "")
-      
-	                //If uploaded file contains zipped files to be unzipped and added to the dataset, wait until the dataset is saved before sending extractor messages to unzip
-	                //and return the files
-	                if(!fileType.equals("multi/files-zipped")){
-				        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, id, host, key, Map.empty, f.length.toString, "", flags))}
-				        //current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",nameOfFile), ("contentType", f.contentType)))}
-			        }
-			        
-			        // add file to dataset 
-			        val dt = dataset.copy(files = List(f), author=identity)
-			        // TODO create a service instead of calling salat directly
-		            Dataset.save(dt)
-		            
-		            if(fileType.equals("multi/files-zipped")){
-				        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, id, host, key, Map.empty, f.length.toString, dt.id.toString, flags))}
-				        //current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",nameOfFile), ("contentType", f.contentType)))}
-			        }
-			        
-		            //index the file
-		            current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",f.filename), ("contentType", fileType),("datasetId",dt.id.toString),("datasetName",dt.name)))}
-		            // index dataset
-		            current.plugin[ElasticsearchPlugin].foreach{_.index("data", "dataset", dt.id.toString, 
-		                List(("name",dt.name), ("description", dt.description)))}
-           
-		            
-			    	// TODO RK need to replace unknown with the server name and dataset type		            
- 			    	val dtkey = "unknown." + "dataset."+ "unknown"
-			        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(dt.id.toString, dt.id.toString, host, dtkey, Map.empty, "0", dt.id.toString, ""))}
-		            // redirect to file page
-		            Redirect(routes.Datasets.dataset(dt.id.toString))
-//		            Ok(views.html.dataset(dt, Previewers.searchFileSystem))
-			      }
-			      
-			      case None => {
-			        Logger.error("Could not retrieve file that was just saved.")
-			        // TODO create a service instead of calling salat directly
-			        val dt = dataset.copy(author=identity)
-		            Dataset.save(dt)
-		            // redirect to file page
-		            Redirect(routes.Datasets.dataset(dt.id.toString))
-//		            Ok(views.html.dataset(dt, Previewers.searchFileSystem))
-			      }
-			    }   
+	             //Uploaded file selected
+	             
+	             //Can't have both an uploaded file and a selected existing file
+	             request.body.asFormUrlEncoded.get("existingFile").get(0).equals("__nofile") match{
+	               case true => {
+	            	    var nameOfFile = f.filename
+			            var flags = ""
+			            if(nameOfFile.endsWith(".ptm")){
+			              var thirdSeparatorIndex = nameOfFile.indexOf("__")
+			              if(thirdSeparatorIndex >= 0){
+			                var firstSeparatorIndex = nameOfFile.indexOf("_")
+			                var secondSeparatorIndex = nameOfFile.indexOf("_", firstSeparatorIndex+1)
+			            	flags = flags + "+numberofIterations_" +  nameOfFile.substring(0,firstSeparatorIndex) + "+heightFactor_" + nameOfFile.substring(firstSeparatorIndex+1,secondSeparatorIndex)+ "+ptm3dDetail_" + nameOfFile.substring(secondSeparatorIndex+1,thirdSeparatorIndex)
+			            	nameOfFile = nameOfFile.substring(thirdSeparatorIndex+2)
+			              }
+			            }
+		
+				        Logger.debug("Uploading file " + nameOfFile)
+				        
+				        // store file
+				        Logger.info("Adding file" + identity)
+				        val showPreviews = request.body.asFormUrlEncoded.get("datasetLevel").get(0)
+					    val file = Services.files.save(new FileInputStream(f.ref.file), nameOfFile, f.contentType, identity, showPreviews)
+					    Logger.debug("Uploaded file id is " + file.get.id)
+					    Logger.debug("Uploaded file type is " + f.contentType)
+					    
+					    val uploadedFile = f
+					    file match {
+					      case Some(f) => {
+					        current.plugin[FileDumpService].foreach{_.dump(DumpOfFile(uploadedFile.ref.file, f.id.toString, nameOfFile))}
+					        
+					        val id = f.id.toString	                	                
+			                if(showPreviews.equals("FileLevel"))
+			                	flags = flags + "+filelevelshowpreviews"
+			                else if(showPreviews.equals("None"))
+			                	flags = flags + "+nopreviews"
+					        var fileType = f.contentType
+					        if(fileType.contains("/zip") || fileType.contains("/x-zip") || nameOfFile.endsWith(".zip")){
+					          fileType = FilesUtils.getMainFileTypeOfZipFile(uploadedFile.ref.file, nameOfFile, "dataset")			          
+					          if(fileType.startsWith("ERROR: ")){
+					             Logger.error(fileType.substring(7))
+					             InternalServerError(fileType.substring(7))
+					          }			          
+					        }else if(nameOfFile.endsWith(".mov")){
+					        	fileType = "ambiguous/mov";
+					        }
+					        
+					        
+					    	// TODO RK need to replace unknown with the server name
+					    	val key = "unknown." + "file."+ fileType.replace(".", "_").replace("/", ".")
+		//			        val key = "unknown." + "file."+ "application.x-ptm"
+					    	
+			                // TODO RK : need figure out if we can use https
+			                val host = "http://" + request.host + request.path.replaceAll("dataset/submit$", "")
+		      
+			                //If uploaded file contains zipped files to be unzipped and added to the dataset, wait until the dataset is saved before sending extractor messages to unzip
+			                //and return the files
+			                if(!fileType.equals("multi/files-zipped")){
+						        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, id, host, key, Map.empty, f.length.toString, "", flags))}
+						        //current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",nameOfFile), ("contentType", f.contentType)))}
+					        }
+					        
+					        // add file to dataset 
+					        val dt = dataset.copy(files = List(f), author=identity)
+					        // TODO create a service instead of calling salat directly
+				            Dataset.save(dt)
+				            
+				            if(fileType.equals("multi/files-zipped")){
+						        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(id, id, host, key, Map.empty, f.length.toString, dt.id.toString, flags))}
+						        //current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",nameOfFile), ("contentType", f.contentType)))}
+					        }
+					        
+				            //index the file
+				            current.plugin[ElasticsearchPlugin].foreach{_.index("data", "file", id, List(("filename",f.filename), ("contentType", fileType),("datasetId",dt.id.toString),("datasetName",dt.name)))}
+				            // index dataset
+				            current.plugin[ElasticsearchPlugin].foreach{_.index("data", "dataset", dt.id.toString, 
+				                List(("name",dt.name), ("description", dt.description)))}
+		           
+				            
+					    	// TODO RK need to replace unknown with the server name and dataset type		            
+		 			    	val dtkey = "unknown." + "dataset."+ "unknown"
+					        current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(dt.id.toString, dt.id.toString, host, dtkey, Map.empty, "0", dt.id.toString, ""))}
+				            // redirect to dataset page
+				            Redirect(routes.Datasets.dataset(dt.id.toString))
+		//		            Ok(views.html.dataset(dt, Previewers.searchFileSystem))
+					      }
+					      
+					      case None => {
+					        Logger.error("Could not retrieve file that was just saved.")
+					        // TODO create a service instead of calling salat directly
+					        val dt = dataset.copy(author=identity)
+				            Dataset.save(dt)
+				            // redirect to dataset page
+				            Redirect(routes.Datasets.dataset(dt.id.toString))
+		//		            Ok(views.html.dataset(dt, Previewers.searchFileSystem))
+					      }
+					    }   	                 
+	                 }
+	               case false => Redirect(routes.Datasets.newDataset()).flashing("error"->"Please select ONE file (upload new or existing)")	
+	               }
+	             
+	           
 	        }.getOrElse{
-	          Redirect(routes.Datasets.newDataset()).flashing("error"->"Please select a file")
+	          val fileId = request.body.asFormUrlEncoded.get("existingFile").get(0)
+	          fileId match{
+	            case "__nofile" => Redirect(routes.Datasets.newDataset()).flashing("error"->"Please select ONE file (upload new or existing)")
+	            case _ => {
+	              //Existing file selected	          
+	          
+		          // add file to dataset 
+		          val theFile = Services.files.getFile(fileId)
+		          if(theFile.isEmpty)
+		            Redirect(routes.Datasets.newDataset()).flashing("error"->"Selected file not found. Maybe it was removed.")
+		          
+				  val dt = dataset.copy(files = List(theFile.get), author=identity)
+				  // TODO create a service instead of calling salat directly
+			      Dataset.save(dt)
+			      
+			      // index dataset
+			      current.plugin[ElasticsearchPlugin].foreach{_.index("data", "dataset", dt.id.toString, 
+			    		  List(("name",dt.name), ("description", dt.description)))}
+		          
+		          // TODO RK : need figure out if we can use https
+		          val host = "http://" + request.host + request.path.replaceAll("dataset/submit$", "")
+				  // TODO RK need to replace unknown with the server name and dataset type		            
+				  val dtkey = "unknown." + "dataset."+ "unknown"
+						  current.plugin[RabbitmqPlugin].foreach{_.extract(ExtractorMessage(dt.id.toString, dt.id.toString, host, dtkey, Map.empty, "0", dt.id.toString, ""))}
+				  // redirect to dataset page
+				  Redirect(routes.Datasets.dataset(dt.id.toString)) 
+	            }	            
+	          }  
 	        }
 		  }
 		)
