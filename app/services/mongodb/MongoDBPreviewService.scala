46620091d5e6f7568fa1a870e7bbc8812ddf24e4
izharnit <inna@illinois.edu>
2014-04-24 12:43:02 -0500

    Added code to store file_id for preview in db. Used by Versus.

--- app/services/mongodb/MongoDBPreviewService.scala
+++ app/services/mongodb/MongoDBPreviewService.scala
@@ -202,7 +202,12 @@ class MongoDBPreviewService @Inject()(files: FileService, tiles: TileService) ex
       case JsObject(fields) => {
         val metadata = fields.toMap.flatMap(tuple => MongoDBObject(tuple._1 -> tuple._2.as[String]))
         PreviewDAO.dao.collection.update(MongoDBObject("_id" -> new ObjectId(previewId.stringify)),
-          $set("metadata" -> metadata, "section_id" -> new ObjectId(metadata("section_id").asInstanceOf[String])), false, false, WriteConcern.Safe)
+          $set("metadata" -> metadata, "section_id" -> new ObjectId(metadata("section_id").asInstanceOf[String]), 
+        		  //the next line adds file_id as string to Preview... used by Versus
+	              "file_id"->metadata("file_id").asInstanceOf[String]),
+	              false, false, WriteConcern.Safe)
+        		/*PreviewDAO.dao.collection.update(MongoDBObject("_id" -> new ObjectId(previewId.stringify)),
+          $set("metadata" -> metadata, "section_id" -> new ObjectId(metadata("section_id").asInstanceOf[String])), false, false, WriteConcern.Safe)*/
         Logger.debug("Updating previews.files " + previewId + " with " + metadata)
       }
       case _ => Logger.error("Expected a JSObject")
