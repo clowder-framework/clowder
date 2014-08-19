9b437f0a89541c52d2354ab6b5bf317586c7bc24
izharnit <inna@illinois.edu>
2014-08-19 15:13:04 -0500

    Deleted commented out code.

--- app/models/VersusIndex.scala
+++ app/models/VersusIndex.scala
@@ -2,7 +2,6 @@
  * Used to pass information about an index between Medici and Versus.
  * Used by VersusPlugin.
  */
-
 package models
 import play.api.libs.json._
 import play.api.libs.json.Reads
@@ -15,33 +14,27 @@ case class VersusIndex(
       val extractorID:String, 
       val measureID:String, 
       val indexerType:String
-/*      val indexName:Option[String],
-      val indexType:Option[String]*/
   )  
   
 object VersusIndex {  
    implicit object Index5 extends Writes[VersusIndex] {
-    	  	
-	def writes(index: VersusIndex) = Json.obj(
-    "id" -> index.id,
-    "mimetype" -> index.MIMEtype,
-    "extr"->index.extractorID,
-    "measure"->index.measureID,
-    "typeOfIndexer"->index.indexerType
-  )
+	   def writes(index: VersusIndex) = Json.obj(
+			"id" -> index.id,
+			"mimetype" -> index.MIMEtype,
+			"extr"->index.extractorID,
+			"measure"->index.measureID,
+			"typeOfIndexer"->index.indexerType
+		)
    }
        
   implicit object Index extends Reads[VersusIndex] {    
-	  	//implicit val format: Format[VersusIndex] = Json.format[VersusIndex]	 	  	
-	def reads(json: JsValue) ={     
-      val maybeID:String = (json \"indexID").as[String]
-      val maybeMimeType:String=(json\"MIMEtype").as[String]
-      val exType:String=(json\"Extractor").as[String]
-      val meType:String=(json\"Measure").as[String]
-      val indxrType:String=(json\"Indexer").as[String]
-     // val maybeName = (json\"indexName").as[Option[String]]
-      //val maybeIndexType = (json\"indexType").as[Option[String]]
-      JsSuccess(VersusIndex(maybeID,maybeMimeType,exType,meType,indxrType/*, maybeName, maybeIndexType*/) )         
+	  def reads(json: JsValue) ={     
+		  val maybeID:String = (json \"indexID").as[String]
+		  val maybeMimeType:String=(json\"MIMEtype").as[String]
+		  val exType:String=(json\"Extractor").as[String]
+		  val meType:String=(json\"Measure").as[String]
+          val indxrType:String=(json\"Indexer").as[String]
+      JsSuccess(VersusIndex(maybeID,maybeMimeType,exType,meType,indxrType))     
     }
    }
 }
