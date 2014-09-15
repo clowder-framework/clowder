ea8f6ea1f8b8aa545eaf20c0db66533a5dd9dbbe
Smruti Padhy <spadhy@illinois.edu>
2014-09-15 13:25:33 -0500

    File upload test is updated and use a test image

--- test/integration/ExtractionFileUploadFunctionSpec.scala
+++ test/integration/ExtractionFileUploadFunctionSpec.scala
@@ -1,6 +1,5 @@
 package integration
 
-
 import play.api.test.FakeApplication
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -8,7 +7,6 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
 import play.api.Logger
 import org.scalatestplus.play.OneAppPerSuite
-import org.scalatest.DoNotDiscover
 import org.scalatestplus.play.PlaySpec
 import play.api.Play
 import org.apache.http.entity.mime.content.ContentBody
@@ -33,8 +31,8 @@ import play.api.http._
  * @author Smruti Padhy
  */
 
-trait FakeMultipartUpload{
-implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[MultipartFormData[TemporaryFile]] = {
+trait FakeMultipartUpload {
+  implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[MultipartFormData[TemporaryFile]] = {
     val builder = MultipartEntityBuilder.create().setBoundary("12345678")
 
     def transform(multipart: MultipartFormData[TemporaryFile]): Array[Byte] = {
@@ -55,8 +53,7 @@ implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[Mul
 
     new Writeable[MultipartFormData[TemporaryFile]](transform, Some(builder.build.getContentType.getValue))
   }
-  
-  
+
   def fileUpload(key: String, file: File, contentType: String): MultipartFormData[TemporaryFile] = {
     MultipartFormData(
       dataParts = Map(),
@@ -64,47 +61,52 @@ implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[Mul
       badParts = Seq(),
       missingFileParts = Seq())
   }
-case class WrappedFakeRequest[A](fr: FakeRequest[A]) {
+  case class WrappedFakeRequest[A](fr: FakeRequest[A]) {
     def withFileUpload(key: String, file: File, contentType: String) = {
       fr.withBody(fileUpload(key, file, contentType))
     }
   }
   implicit def toWrappedFakeRequest[A](fr: FakeRequest[A]) = WrappedFakeRequest(fr)
 }
-
-
-
-@DoNotDiscover
-class ExtractionFileUploadFunctionSpec extends PlaySpec with OneAppPerSuite with FakeMultipartUpload{
- val excludedPlugins = List(
+class ExtractionFileUploadFunctionSpec extends PlaySpec with OneAppPerSuite with FakeMultipartUpload {
+  val excludedPlugins = List(
     "services.RabbitmqPlugin",
     "services.VersusPlugin")
- 
+
   implicit override lazy val app: FakeApplication = FakeApplication(withoutPlugins = excludedPlugins)
- 	
- "The OneAppPerSuite for Extraction API Controller Router test" must { 
+
+  "The OneAppPerSuite for Extraction API Controller Router test" must {
     "respond to the Upload File URL" in {
-      val request = FakeRequest(POST,"/api/extractions/upload_url?key=r1ek3rs").withJsonBody(Json.toJson(Map("fileurl"->"http://www.ncsa.illinois.edu/assets/img/logos_ncsa.png")))
+      val secretKey = play.api.Play.configuration.getString("commKey").getOrElse("")
+      val fileurl = "http://www.ncsa.illinois.edu/assets/img/logos_ncsa.png"
+      val request = FakeRequest(POST, "/api/extractions/upload_url?key=" + secretKey).withJsonBody(Json.toJson(Map("fileurl" -> fileurl)))
       val result = route(request).get
-      info("Status="+status(result))
+      info("Status=" + status(result))
       status(result) mustEqual OK
-      info("contentType="+contentType(result))
+      info("contentType=" + contentType(result))
       contentType(result) mustEqual Some("application/json")
-      
+      contentAsString(result) must include ("id")
+      info("contentAsString"+ contentAsString(result))
+
     }
-     "respond to the Upload File" in {
-        val file1=new java.io.File("/Users/smruti/Desktop/testpic/image.jpg")
-        if(file1.isFile && file1.exists){
-          Logger.debug("File1 is File:True")
-        }
-        val req = FakeRequest(POST,"/api/extractions/upload_file?key=r1ek3rs" ).
-          withFileUpload("File", file1, "image/jpg")
-        val result = route(req).get
-         
-      info("Status="+status(result))
+    "respond to the Upload File" in {
+      val secretKey = play.api.Play.configuration.getString("commKey").getOrElse("")
+      val workingDir = System.getProperty("user.dir")
+      info("Working Directory: " + workingDir)
+      val file1 = new java.io.File(workingDir + "/test/data/morrowplots.jpg")
+      if (file1.isFile && file1.exists) {
+        Logger.debug("File1 is File:True")
+      }
+      val req = FakeRequest(POST, "/api/extractions/upload_file?key=" + secretKey).
+        withFileUpload("File", file1, "image/jpg")
+      val result = route(req).get
+
+      info("Status=" + status(result))
       status(result) mustEqual OK
-      info("contentType="+contentType(result))
+      info("contentType=" + contentType(result))
       contentType(result) mustEqual Some("application/json")
+      contentAsString(result) must include ("id")
+      info("contentAsString"+ contentAsString(result))
     }
 
   }
