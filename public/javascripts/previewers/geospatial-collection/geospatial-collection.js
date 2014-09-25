8883c3c3a4b4678ed7e3d1d35ec324be6e9082b1
Jong Lee <jonglee1@illinois.edu>
2014-09-25 14:42:26 -0500

    added comments

--- public/javascripts/previewers/geospatial-collection/geospatial-collection.js
+++ public/javascripts/previewers/geospatial-collection/geospatial-collection.js
@@ -23,11 +23,13 @@
 			var mapDiv = "<div id='map' style='height:400px;width:100%'></div>"
 			$(Configuration.tab).append(mapDiv);
 			
+			// adding div for layer control
 			var layerControlDiv = "<div id='toolbox' style='position:absolute; top:45px; right:20px; padding:3px; border-radius:4px; color:#fff; background: rgba(255, 255, 255, 0.4); z-index:100;' >";
 			layerControlDiv += "<div id='control-title' style='color:black; cursor:pointer;'><span class='glyphicon glyphicon-tasks'></span>&nbsp;Geospatial Layers</div>";
 			layerControlDiv += "<div id='layer-control' style='margin:0; padding:10px; border-radius:4px; background:rgba(0, 60, 136, 0.5);'></div></div>";
 			$(Configuration.tab).append(layerControlDiv);
 
+			// adding event handler for collapsible div
 			$('#control-title').click(function(){
 	            $('#layer-control').toggle();
 	        });
@@ -51,6 +53,8 @@
 				// looping through the datasets
 				for (var i=0;i < data.length; i ++) {
 					var dataset_id = data[i]['id'];
+					
+					// this variable will be placed in $.ajax call to be used in .done function
 					var dataset_title = data[i]['datasetname'];
 					
 					// setting up ajax call to get file from the dataset
@@ -62,7 +66,9 @@
 					});
 					
 					file_req.done(function(data){
+						// this variable will be placed in $.ajax call to be used in .done function
 						var title = this.datasetTitle;
+						
 						var file_id = data[0]['id'];
 						
 						// setting up ajax call to get metadata from the file
@@ -73,7 +79,9 @@
 							dataType: "json"
 						});
 						meta_req.done(function(data){
+							// this variable will be placed in $.ajax call to be used in .done function
 							var title = this.datasetTitle;
+							
 							// skip files without wms metadata
 							if (data == null) {
 								console.log("NO - null");
@@ -97,6 +105,8 @@
 							var wmsUrl = data[0]["WMS Service URL"];
 							var layerName = data[0]["WMS Layer Name"];
 							var wmsLayerUrl = data[0]["WMS Layer URL"];
+							
+							// remove the 'wssi:' (workspace prefix) 
 							var name = layerName.split(":")
 							
 							// extract extent from WMS layer url
@@ -152,11 +162,17 @@
 							// add wms layer to the map
 							map.addLayer(wmsLayer);
 							
-							// populate layer control for each layer
+							// Populate layer control for each layer
+							
+							// create id by using wms layer name
 							var visVar = name[1]+'-visible'; 
 							var opVar = name[1]+'-opacity';
+							
+							// add checkbox and range input
 							var layerControl = '<div><input id="'+visVar+'" type="checkbox" checked="checked" />'+title+':&nbsp;&nbsp';
 							layerControl += '<input id="'+opVar+'" type="range" min="0" max="1" step="0.01" value="1" style="width:100px;"/></div>';
+							
+							// prepend the layer not "append" since the top item means the layer on top
 							$('#layer-control').prepend(layerControl);
 							
 							// event handler for layer on/off
