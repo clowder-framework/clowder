7bcb62e4c2bb3e2d37f70472d7b003949c10794f
Jong Lee <jonglee1@illinois.edu>
2014-09-25 14:30:07 -0500

    added layer control

--- public/javascripts/previewers/geospatial-collection/geospatial-collection.js
+++ public/javascripts/previewers/geospatial-collection/geospatial-collection.js
@@ -23,6 +23,15 @@
 			var mapDiv = "<div id='map' style='height:400px;width:100%'></div>"
 			$(Configuration.tab).append(mapDiv);
 			
+			var layerControlDiv = "<div id='toolbox' style='position:absolute; top:45px; right:20px; padding:3px; border-radius:4px; color:#fff; background: rgba(255, 255, 255, 0.4); z-index:100;' >";
+			layerControlDiv += "<div id='control-title' style='color:black;'><span class='glyphicon glyphicon-tasks'></span>&nbsp;Geospatial Layers</div>";
+			layerControlDiv += "<div id='layer-control' style='margin:0; padding:10px; border-radius:4px; background:rgba(0, 60, 136, 0.5);'></div></div>";
+			$(Configuration.tab).append(layerControlDiv);
+
+			$('#control-title').click(function(){
+	            $('#layer-control').toggle();
+	        });
+			
 			// loading the ol3 script
 			$.getScript("http://openlayers.org/en/v3.0.0/build/ol.js", function() {
 				// initiating map 
@@ -42,24 +51,29 @@
 				// looping through the datasets
 				for (var i=0;i < data.length; i ++) {
 					var dataset_id = data[i]['id'];
+					var dataset_title = data[i]['datasetname'];
 					
 					// setting up ajax call to get file from the dataset
 					var file_req = $.ajax({
+						datasetTitle: dataset_title,
 						type: "GET",
 						url: "/api/datasets/"+dataset_id+"/listFiles",
 						dataType: "json"
 					});
 					
 					file_req.done(function(data){
+						var title = this.datasetTitle;
 						var file_id = data[0]['id'];
 						
 						// setting up ajax call to get metadata from the file
 						var meta_req = $.ajax({
+							datasetTitle: title,
 							type: "GET",
 							url: "/api/files/"+file_id+"/technicalmetadatajson",
 							dataType: "json"
 						});
 						meta_req.done(function(data){
+							var title = this.datasetTitle;
 							// skip files without wms metadata
 							if (data == null) {
 								console.log("NO - null");
@@ -83,7 +97,8 @@
 							var wmsUrl = data[0]["WMS Service URL"];
 							var layerName = data[0]["WMS Layer Name"];
 							var wmsLayerUrl = data[0]["WMS Layer URL"];
-
+							var name = layerName.split(":")
+							
 							// extract extent from WMS layer url
 							var parser = document.createElement('a'), searchObject = {}, queries, split;
 							parser.href = wmsLayerUrl;
@@ -137,6 +152,28 @@
 							// add wms layer to the map
 							map.addLayer(wmsLayer);
 							
+							// populate layer control for each layer
+							var visVar = name[1]+'-visible'; 
+							var opVar = name[1]+'-opacity';
+							var layerControl = '<div><input id="'+visVar+'" type="checkbox" checked="checked" />'+title+':&nbsp;&nbsp';
+							layerControl += '<input id="'+opVar+'" type="range" min="0" max="1" step="0.01" value="1" style="width:100px;"/></div>';
+							$('#layer-control').prepend(layerControl);
+							
+							// event handler for layer on/off
+							$("#"+visVar).change(function() {
+								if($(this).is(':checked')){
+									wmsLayer.setVisible(true);
+								} else {	
+									wmsLayer.setVisible(false);
+								}
+							});
+							
+							// event handler for layer opacity
+							$("#"+opVar).change(function() {
+								wmsLayer.setOpacity($(this).val());
+							});
+							
+							
 							// retrieving the map div size for fit
 //							zheight = $('#map').height() - 100;
 //							zwidth = $('#map').width();
@@ -144,7 +181,7 @@
 //							console.log('extent:', current_coord);
 //							console.log('zoom with h:' + zheight
 //									+ ', w:' + zwidth);
-
+							
 							// create view object to zoom in
 							var view = new ol.View();
 							view.fitExtent(current_coord, map.getSize());
