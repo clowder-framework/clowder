cf9eeca2bf14b8caf102454b517bbd612106d5ae
Rob Kooper <kooper@illinois.edu>
2014-10-17 13:55:57 -0700

    simplified mongo plugin
    Will connect to database based on following keys:
    1) connect to mongodbURI
    2) will fall back on mongodb.default
    3) finally will use mongodb://127.0.0.1:27017/medici

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,5 @@
-import com.mongodb.casbah.Imports._
 import play.api.{GlobalSettings, Application}
 import play.api.Logger
-import play.api.Play.current
 import play.libs.Akka
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -9,7 +7,6 @@ import models.ExtractionInfoSetUp
 import java.util.Date
 import java.util.Calendar
 import models._
-import services.mongodb.MongoSalatPlugin
 import play.api.mvc.WithFilters
 import play.filters.gzip.GzipFilter
 import akka.actor.Cancellable
@@ -28,28 +25,12 @@ object Global extends WithFilters(new GzipFilter(),CORSFilter()) with GlobalSett
   var extractorTimer: Cancellable = null
 
   override def onStart(app: Application) {
-    // create mongo indexes if plugin is loaded
     ServerStartTime.startTime = Calendar.getInstance().getTime
     serverStartTime = ServerStartTime.startTime
     Logger.debug("\n----Server Start Time----" + serverStartTime + "\n \n")
-    
-    current.plugin[MongoSalatPlugin].map { mongo =>
-      mongo.sources.values.map { source =>
-        Logger.debug("Ensuring indexes on " + source.uri)
-        source.collection("datasets").ensureIndex(MongoDBObject("created" -> -1))
-        source.collection("datasets").ensureIndex(MongoDBObject("tags" -> 1))
-        source.collection("uploads.files").ensureIndex(MongoDBObject("uploadDate" -> -1))
-        source.collection("uploadquery.files").ensureIndex(MongoDBObject("uploadDate" -> -1))
-        source.collection("previews.files").ensureIndex(MongoDBObject("uploadDate" -> -1, "file_id" -> 1))
-        source.collection("previews.files").ensureIndex(MongoDBObject("uploadDate" -> -1, "section_id" -> 1))
-        source.collection("sections").ensureIndex(MongoDBObject("uploadDate" -> -1, "file_id" -> 1))
-        source.collection("dtsrequests").ensureIndex(MongoDBObject("startTime" -> -1, "endTime" -> -1))
-        source.collection("versus.descriptors").ensureIndex(MongoDBObject("fileId" -> 1))
-      }
 
-    }
-        
     //Add permanent admins to app if not already included
+    // TODO this should be a service so we can store it in other places than mongo
     val appConfObj = new services.mongodb.MongoDBAppConfigurationService{}    
     appConfObj.getDefault()
     for(initialAdmin <- play.Play.application().configuration().getString("initialAdmins").split(","))
