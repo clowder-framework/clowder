482610a0fca1ec405fc7cc1e3f28992bf3c13d39
Rob Kooper <kooper@illinois.edu>
2014-12-12 15:10:50 -0800

    allow folders with resources
    This changes the medici-play.bat and medici-play startup scripts to be
    a lot simpler. They now allow to specify a variable MEDICI_CLASSPATH
    which can be used to add additional jar files and/or folders to the
    class path.

--- app/util/ResourceLister.scala
+++ app/util/ResourceLister.scala
@@ -1,13 +1,11 @@
 package util
 
-import java.io.{File, FileFilter}
 import java.util.regex.Pattern
 
 import org.reflections.Reflections
 import org.reflections.scanners.ResourcesScanner
-import org.reflections.util.ConfigurationBuilder
-import play.api.{Logger, Play}
-import play.api.Play.current
+import org.reflections.util.{FilterBuilder, ClasspathHelper, ConfigurationBuilder}
+import play.api.Logger
 
 import scala.collection.JavaConversions
 
@@ -15,15 +13,25 @@ import scala.collection.JavaConversions
  * @author Rob Kooper
  */
 object ResourceLister {
+  /**
+   * Search the classpath for files with the given path and filename. The path
+   * should be specified with a . instead of the path separator, allowing it to
+   * find files inside a jar file as well.
+   */
   def listFiles(path: String, regex: String): List[String] = {
     try {
-      val configuration = ConfigurationBuilder.build("", new ResourcesScanner())
-      val reflections = new Reflections(configuration)
-      val files = JavaConversions.asScalaSet(reflections.getResources(Pattern.compile(regex)))
-        .filter(s => s.startsWith(path))
-        .toList
-        .sorted
-      Logger.debug("Found files : " + files)
+      val startTime = System.currentTimeMillis()
+
+      // Create the default resource checker
+      val configBuilder = new ConfigurationBuilder()
+        .filterInputsBy(new FilterBuilder().include(path + ".*"))
+        .setUrls(ClasspathHelper.forClassLoader())
+        .setScanners(new ResourcesScanner())
+
+      // Find all resources
+      val reflections = new Reflections(configBuilder)
+      val files = JavaConversions.asScalaSet(reflections.getResources(Pattern.compile(regex))).toList.sorted
+      Logger.debug("Found " + files.length + " files in " + (System.currentTimeMillis() - startTime) + " ms.")
       files
     } catch {
       case t:Throwable => {
