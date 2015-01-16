34ec5f8c8f1f82bf50b6f5406ab7e212f9cfba71
c.sophocleous <c.sophocleous@cyi.ac.cy>
2015-01-16 16:46:42 +0200

    Video previewers changed to reflect the video finally being fixed and working properly without browser plugins using Popcorn.js and WebM. Quicktime to be used for QTVR mov and video mov with not-yet-extracted previews and popcorn.js as the sole permanent video viewer.

--- public/javascripts/previewers/popcorn/popcorn.js
+++ public/javascripts/previewers/popcorn/popcorn.js
@@ -3,24 +3,13 @@
   
   var s = document.createElement("script");
   s.type = "text/javascript";
-  s.src = "http://popcornjs.org/code/dist/popcorn-complete.min.js";
+  s.src = window.location.protocol + "//popcornjs.org/code/dist/popcorn-complete.min.js";
     
   console.log("Updating tab " + Configuration.tab);
   $(Configuration.tab).append(s);
+  $(Configuration.tab).append("<br/>");
   $(Configuration.tab).append(			  
-     "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
+     "<video width='750px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
   );
-  
-//  var height = 400;  
-//  var width = 750;
-//  
-//  $(Configuration.tab).append("<p>For panoramic images viewed through Quicktime, Shift can be used to zoom in and Ctrl to zoom out.</p>");
-//    
-//  console.log("Updating tab " + Configuration.tab);
-//  $(Configuration.tab).append(s);  
-//  $(Configuration.tab).append(			  
-//     "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
-//  );
-  
-  
+    
 }(jQuery, Configuration));
