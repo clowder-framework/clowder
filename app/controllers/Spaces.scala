dec1adc37e85c75a8b4ca2214717435bb2cacb5b
Rob Kooper <kooper@illinois.edu>
2015-01-26 17:00:21 -0600

    Add Controller to api/Spaces and controllers/Spaces

--- app/controllers/Spaces.scala
+++ app/controllers/Spaces.scala
@@ -2,14 +2,17 @@ package controllers
 
 import javax.inject.Inject
 
+import api.{Permission, WithPermission}
+import models.UUID
+import play.api.Logger
 import services.SpaceService
 
 /**
  * Spaces allow users to partition the data into realms only accessible to users with the right permissions.
  *
  * @author Luigi Marini
+ * @author Rob Kooper
  *
  */
-class Spaces @Inject()(spaces: SpaceService) {
-
+class Spaces @Inject()(spaces: SpaceService) extends SecuredController {
 }
