0c99c2632d7e9b9a56b0e7eaaefeafde5701e01b
Jong Lee <jonglee1@illinois.edu>
2015-01-28 11:58:10 -0600

    SEAD-713 removed the unused function

--- public/javascripts/spaceListProcess.js
+++ public/javascripts/spaceListProcess.js
@@ -1,32 +1,8 @@
 //@author: Jong Lee (jonglee)
-//TODO: the code needs to be refactored based on the api implementation
-
-//Method to remove the space
-function removeSpace(spaceId,event){
-	var request = jsRoutes.api.Spaces.removeSpace(spaceId).ajax({
-		type: 'POST'
-	});
-	request.done(function (response, textStatus, jqXHR){
-        console.log("Response " + response);
-        if($(event.target).is("span")){
-        	$(event.target.parentNode.parentNode.parentNode).remove();
-        }
-        else{
-        	$(event.target.parentNode.parentNode).remove();
-        }    
-    });
-	request.fail(function (jqXHR, textStatus, errorThrown){
-		console.error("The following error occurred: "+textStatus, errorThrown);
-        var errMsg = "You must be logged in to remove a collection from the system.";
-        if (!checkErrorAndRedirect(jqXHR, errMsg)) {
-            alert("The collection was not removed due to : " + errorThrown);
-        }   		
-	});
-}
 
 //Method to remove the space and redirect back to a specific URL on completion
 function removeSpaceAndRedirect(spaceId, url){
-	if(url === undefined) reloadPage = "/spaces";
+	if(url === undefined) url = "/spaces";
 	
 	var request = jsRoutes.api.Spaces.removeSpace(spaceId).ajax({
 		type: 'DELETE'
