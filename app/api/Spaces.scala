7b6bd0b412a1f136eacf640f8559a29451d5ff96
Jong Lee <jonglee1@illinois.edu>
2015-01-28 11:45:29 -0600

    SEAD-713 implemented REST api for removing a space

--- app/api/Spaces.scala
+++ app/api/Spaces.scala
@@ -14,6 +14,7 @@ import services.{AdminsNotifierPlugin, SpaceService}
  *
  * @author Luigi Marini
  * @author Rob Kooper
+ * @author Jong Lee
  *
  */
 @Api(value = "/spaces", listingPath = "/api-docs.json/spaces", description = "Spaces are groupings of collections and datasets.")
