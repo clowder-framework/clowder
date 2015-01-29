e333ce97418510f14e2ae374e661b7bab12c1b0c
Ashwini Vaidya <vaidya10@illinois.edu>
2015-01-29 08:51:15 -0600

    creator in, api/spaces/create space in

--- app/api/Spaces.scala
+++ app/api/Spaces.scala
@@ -1,8 +1,12 @@
 package api
 
+import java.util.Date
 import javax.inject.Inject
 
 import com.wordnik.swagger.annotations.{ApiOperation, Api}
+import models.{UUID, ProjectSpace, Collection}
+import play.api.Logger
+import play.api.libs.json.Json._
 import services.SpaceService
 
 /**
@@ -14,4 +18,28 @@ import services.SpaceService
  */
 @Api(value = "/spaces", listingPath = "/api-docs.json/spaces", description = "Spaces are groupings of collections and datasets.")
 class Spaces @Inject()(spaces: SpaceService) extends ApiController {
+  @ApiOperation(value = "Create a space",
+    notes = "",
+    responseClass = "None", httpMethod = "POST")
+  def createSpace() = SecuredAction(authorization=WithPermission(Permission.CreateSpaces)) {
+    request =>
+      Logger.debug("Creating new space")
+      val nameOpt = (request.body \ "name").asOpt[String]
+      val descOpt = (request.body \ "description").asOpt[String]
+      (nameOpt, descOpt) match{
+        case(Some(name), Some(description)) =>{
+          val c = ProjectSpace(name = name, description = description, created = new Date(), creator = (UUID.apply(""),""),
+            homePage = List.empty, logoURL = None, bannerURL = None, usersByRole= Map.empty, collectionCount=0, datasetCount=0, userCount=0, metadata=List.empty)
+          spaces.insert(c) match {
+            case Some(id) => {
+              Ok(toJson(Map("id" -> id)))
+            }
+            case None => Ok(toJson(Map("status" -> "error")))
+          }
+
+        }
+        case (_,_) =>BadRequest(toJson("Missing required parameters"))
+      }
+  }
+
 }
--- app/controllers/Spaces.scala
+++ app/controllers/Spaces.scala
@@ -5,10 +5,11 @@ import javax.inject.Inject
 import api.{Permission, WithPermission}
 import models.{ProjectSpace, UUID}
 import play.api.Logger
-import play.api.data.Form
+import play.api.data.{Forms, Form}
 import play.api.data.Forms._
+import play.api.data.format.Formats._
 import java.util.Date
-import services.SpaceService
+import services.{UserService, SpaceService}
 
 /**
  * Spaces allow users to partition the data into realms only accessible to users with the right permissions.
@@ -17,18 +18,21 @@ import services.SpaceService
  * @author Rob Kooper
  *
  */
-class Spaces @Inject()(spaces: SpaceService) extends SecuredController {
+class Spaces @Inject()(spaces: SpaceService, users: UserService) extends SecuredController {
   /**
    * New project space form.
    */
+
   val spaceForm = Form(
     mapping(
       "name" -> nonEmptyText,
-      "description" -> nonEmptyText
+      "description" -> nonEmptyText,
+      "logoUrl" -> optional(Utils.CustomMappings.urlType),
+      "bannerUrl" -> optional(Utils.CustomMappings.urlType)
     )
-      ((name, description) => ProjectSpace(name = name, description = description, created = new Date, creator = (UUID.apply(""),""),
-          homePage = List.empty, logoURL = None, bannerURL = None, usersByRole= Map.empty, collectionCount=0, datasetCount=0, userCount=0, metadata=List.empty))
-      ((space: ProjectSpace) => Some((space.name, space.description)))
+      ((name, description, logoUrl, bannerUrl) => ProjectSpace(name = name, description = description, created = new Date, creator = (UUID.apply(""),""),
+          homePage = List.empty, logoURL = logoUrl, bannerURL = bannerUrl, usersByRole= Map.empty, collectionCount=0, datasetCount=0, userCount=0, metadata=List.empty))
+      ((space: ProjectSpace) => Some((space.name, space.description, space.logoURL, space.bannerURL)))
   )
 
   def newSpace() = SecuredAction(authorization = WithPermission(Permission.CreateSpaces)) {
@@ -50,8 +54,18 @@ class Spaces @Inject()(spaces: SpaceService) extends SecuredController {
             errors => BadRequest(views.html.newSpace(errors)),
             space => {
               Logger.debug("Saving space " + space.name)
-              spaces.insert(ProjectSpace(id = space.id, name = space.name, description = space.description, created = space.created, creator = (UUID.apply(""),""),
-                homePage = List.empty, logoURL = None, bannerURL = None, usersByRole= Map.empty, collectionCount=0, datasetCount=0, userCount=0, metadata=List.empty))
+
+              val (id, name) = identity.email match{
+                case Some(userEmail)=>{
+                  val cr = users.findByEmail(userEmail).get
+                  (cr.id, cr.fullName)
+                }
+                case None =>{(UUID.apply(""), "")}
+              }
+
+              //cant handle URL's yet
+              spaces.insert(ProjectSpace(id = space.id, name = space.name, description = space.description, created = space.created, creator = (id, name),
+                homePage = List.empty, logoURL = None/*space.logoURL*/, bannerURL = None /*space.bannerURL*/, usersByRole= Map.empty, collectionCount=0, datasetCount=0, userCount=0, metadata=List.empty))
 
               // index collection
               // val dateFormat = new SimpleDateFormat("dd/MM/yyyy")
