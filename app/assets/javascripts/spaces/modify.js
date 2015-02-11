90348706aaad8ef9f5bc96bc7838cda0de463364
Luigi Marini <lmarini@illinois.edu>
2015-02-11 10:25:52 -0600

    Static assign collection to space using handlebars templates

--- app/assets/javascripts/spaces/modify.js
+++ app/assets/javascripts/spaces/modify.js
@@ -3,6 +3,10 @@
  */
 function changeSpace() {
     console.log('changing space');
+    var modalTemplate = Handlebars.getTemplate('/assets/templates/spaces/assign');
+    var html = modalTemplate({resource_type : "Collection"});
+    $('.container').append(html);
+    $('#spaces-assign').modal();
 }
 
 window['changeSpace'] = changeSpace;
