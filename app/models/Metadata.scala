8df3fdcb80ea68e24bdc2980ce6e6de31694272e
Smruti Padhy <spadhy@illinois.edu>
2015-02-25 15:09:53 -0600

    No need of Json serializer as the fields are basic types and for URL type, in mongodb, corresponding Json serializer/deserializer has been added

--- app/models/Metadata.scala
+++ app/models/Metadata.scala
@@ -15,7 +15,8 @@ import play.api.libs.json.Json
  **/
 case class Metadata (
   id : UUID = UUID.generate,
-  attachedTo: Map[String, UUID], //metadata attached to a preview/section/file/dataset/collection/space, e.g. (file_id->UUID)
+  attachedTo: Map[String, UUID], //metadata attached to an ElementType e.g. file, dataset, collection, space
+                                 //e.g. (file_id->UUID)
   contextId : Option[UUID] = None,     
   createdAt : Date,
   creator : Agent,
@@ -31,44 +32,11 @@ trait Agent {
 
 // User through the GUI
 case class UserAgent( id: UUID, var typeOfAgent : String = "user", userId: Option[URL]) extends Agent
-//case class UserAgent( id: UUID, var typeOfAgent : String = "user", userId: Option[String]) extends Agent
 
 // Automatic extraction
 case class ExtractorAgent( id: UUID, var typeOfAgent : String = "extractor" , extractorId: Option[URL]) extends Agent
-//case class ExtractorAgent( id: UUID, var typeOfAgent : String = "extractor" , extractorId: Option[String]) extends Agent
 
-object UserAgent  {
-  implicit val UserAgentWrites = new Writes[UserAgent] {
-    def writes(agent: UserAgent) = Json.obj(
-      "id" -> agent.typeOfAgent,
-      "typeOfAgent" -> agent.typeOfAgent,
-      "userId" -> agent.userId.toString
-   )
-  }
-  
-}
 
-object Agent{
-implicit val AgentWrites = new Writes[Agent] {
-    def writes(agent: Agent) = Json.obj(
-      "id" -> agent.typeOfAgent,
-      "typeOfAgent" -> agent.typeOfAgent    
-   )
-  }
-}
 
-object Metadata{
-  implicit val MetadataWrites = new Writes[Metadata] {
-    def writes(metadata: Metadata) = Json.obj(
-      "id" -> metadata.id,
-      "attachedTo"-> metadata.attachedTo, 
-      "contextId" -> metadata.contextId,
-      "createdAt" -> metadata.createdAt,
-      "creator" -> metadata.creator,
-      "content"  -> metadata.content,
-      "version" -> metadata.version
-   )
-  }
-}
 
 
