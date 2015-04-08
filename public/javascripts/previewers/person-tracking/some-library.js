7e5ee94e37e959c65e8c9844c8456d052057a4a4
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2015-04-08 14:26:07 -0500

    Bug fix in data reduction code, UI changes, code cleanup

--- public/javascripts/previewers/person-tracking/some-library.js
+++ public/javascripts/previewers/person-tracking/some-library.js
@@ -27,6 +27,7 @@
         var pathNavigateJS = pathJs + "jquery.flot.navigate.js";
         var pathCrosshairJS = pathJs + "jquery.flot.crosshair.js";
         var pathPopcornJS = pathJs + "popcorn-complete.min.js";
+        var dataJS = pathJs + "sample.js";
         var sortedFrameDataArray = new Array();
 
         //dowload JQuery library files        	        	        
