9907039780cf9900a27bfe93f96117a4274922fc
Rob Kooper <kooper@illinois.edu>
2015-05-06 11:38:27 -0500

    use regular file instead of tempfile MMDB-1839
    TempFile will be deleted after a garbage collect.

--- test/integration/utilities/MultiFileUpload.scala
+++ test/integration/utilities/MultiFileUpload.scala
@@ -1,45 +1,23 @@
 package integration
 
-import play.api.test.FakeApplication
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.Json
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import play.api.Logger
-import org.scalatestplus.play.OneAppPerSuite
-import org.scalatestplus.play.PlaySpec
-import play.api.Play
-import org.apache.http.entity.mime.content.ContentBody
-import org.apache.http.entity.mime.MultipartEntity
 import org.apache.http.entity.mime.MultipartEntityBuilder
 import org.apache.commons.io.output.ByteArrayOutputStream
 import org.apache.http.entity.mime.content.FileBody
-import play.api.libs.json.JsObject
-import scala.io.Source
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.FileReader
 import java.io.File
 import play.api.http.Writeable
 import play.api.mvc.MultipartFormData
-import play.api.libs.Files.TemporaryFile
 import play.api.mvc.MultipartFormData.FilePart
 import play.api.mvc.Codec
 import org.apache.http.entity.ContentType
 
-
-
-
-
 trait FakeMultipartUpload {
-  implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[MultipartFormData[TemporaryFile]] = {
+  implicit def writeableOf_multiPartFormData(implicit codec: Codec): Writeable[MultipartFormData[File]] = {
     val builder = MultipartEntityBuilder.create().setBoundary("--boundary---")
 
-    def transform(multipart: MultipartFormData[TemporaryFile]): Array[Byte] = {
+    def transform(multipart: MultipartFormData[File]): Array[Byte] = {
       multipart.files.foreach { file =>
-        val part = new FileBody(file.ref.file, ContentType.create(file.contentType.getOrElse("application/octet-stream")), file.filename)
+        val part = new FileBody(file.ref)
         builder.addPart(file.key, part)
       }
 
@@ -48,13 +26,13 @@ trait FakeMultipartUpload {
       outputStream.toByteArray
     }
 
-    new Writeable[MultipartFormData[TemporaryFile]](transform, Some(builder.build.getContentType.getValue))
+    new Writeable[MultipartFormData[File]](transform, Some(builder.build.getContentType.getValue))
   }
 
-  def fileUpload(key: String, file: File, contentType: String): MultipartFormData[TemporaryFile] = {
+  def fileUpload(key: String, file: File, contentType: String): MultipartFormData[File] = {
     MultipartFormData(
       dataParts = Map(),
-      files = Seq(FilePart[TemporaryFile](key, file.getName, Some(contentType), TemporaryFile(file))),
+      files = Seq(FilePart[File](key, file.getName, Some(contentType), file)),
       badParts = Seq(),
       missingFileParts = Seq())
   }
