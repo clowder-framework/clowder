e6c4fabd638f8ff0ea803b37a687d4a16e9354b3
Rob Kooper <kooper@illinois.edu>
2015-05-26 14:43:59 -0500

    create default permissions
    These are only created if the roles do not exist.

--- app/Global.scala
+++ app/Global.scala
@@ -1,13 +1,14 @@
+import api.Permission
 import play.api.{GlobalSettings, Application}
 import play.api.Logger
 
 import play.filters.gzip.GzipFilter
 
 import play.libs.Akka
-import services.AppConfiguration
+import services.{UserService, DI, AppConfiguration}
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits._
-import models.{ServerStartTime, CORSFilter, ExtractionInfoSetUp}
+import models.{Role, ServerStartTime, CORSFilter, ExtractionInfoSetUp}
 import java.util.Calendar
 import play.api.mvc.WithFilters
 import akka.actor.Cancellable
@@ -27,6 +28,34 @@ object Global extends WithFilters(new GzipFilter(), new Jsonp(), CORSFilter()) w
 
     // set admins
     AppConfiguration.setDefaultAdmins()
+
+    // create default roles
+    val users: UserService = DI.injector.getInstance(classOf[UserService])
+    if (users.listRoles().size == 0) {
+      Logger.debug("Ensuring roles exist")
+
+      // admin role
+      val adminPerm = Permission.values
+      val adminRole = new Role(name="Admin", description="Admin Role", permissions = adminPerm.map(_.toString).toList)
+      users.updateRole(adminRole)
+
+      // editor role
+      val editorPerm = for(perm <- adminPerm if perm.toString.toLowerCase.indexOf("admin") == -1) yield perm
+      val editorRole = new Role(name="Editor", description="Editor Role", permissions = editorPerm.map(_.toString).toList)
+      users.updateRole(editorRole)
+
+      // viewer role
+      val viewerPerm = List(Permission.Public,
+        Permission.ListCollections, Permission.ShowCollection, Permission.ListSpaces, Permission.ShowSpace,
+        Permission.ListDatasets, Permission.ShowDataset, Permission.SearchDatasets, Permission.ShowDatasetsMetadata,
+        Permission.ShowTags, Permission.CreateComments, Permission.AddSections, Permission.GetSections,
+        Permission.CreateTagsSections, Permission.ListFiles, Permission.ShowFilesMetadata, Permission.ShowFile,
+        Permission.SearchFiles, Permission.CreateTagsFiles, Permission.SearchStreams, Permission.ListSensors,
+        Permission.GetSensors, Permission.SearchSensors, Permission.DownloadFiles)
+      val viewerRole = new Role(name="Viewer", description="Viewer Role", permissions = viewerPerm.map(_.toString).toList)
+      users.updateRole(viewerRole)
+    }
+
 
     extractorTimer = Akka.system().scheduler.schedule(0 minutes, 5 minutes) {
       ExtractionInfoSetUp.updateExtractorsInfo()
