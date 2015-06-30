e0bf00d3d03619041a274739a709257577eb2c59
Mario Felarca <gennadius@yahoo.com>
2015-06-30 17:18:18 -0500

    Merge from spaces+cs492. Forgot to add in the new, untracked files.

---
+++ app/api/Events.scala
@@ -0,0 +1,30 @@
+package api
+
+import play.api.Logger
+import play.api.Play.current
+import models.{UUID, Collection, Event}
+import services._
+import play.api.libs.json.{JsObject, JsValue}
+import play.api.libs.json.Json.toJson
+import javax.inject.{ Singleton, Inject }
+import scala.util.{Try, Success, Failure}
+import com.wordnik.swagger.annotations.Api
+import com.wordnik.swagger.annotations.ApiOperation
+import java.util.Date
+import controllers.Utils
+
+
+class Events @Inject() (events: EventService) extends ApiController {
+
+  /*
+   * Add a new event to the database
+   */
+     @ApiOperation(value = "Insert Event",
+      notes = "Insert an Event into the Events Collection",
+      responseClass = "None", httpMethod = "POST")
+  def addEvent(event: Event) = AuthenticatedAction {implicit request =>
+      events.addEvent(event)
+      Ok(toJson("added new event"))
+  }
+
+}
