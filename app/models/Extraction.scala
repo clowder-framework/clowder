fc54da0fc57ed3a6d5805142221cd835469d0924
Luigi Marini <lmarini@illinois.edu>
2015-07-06 15:48:49 -0500

    Added ExtractionInfo model.

--- app/models/Extraction.scala
+++ app/models/Extraction.scala
@@ -1,12 +1,10 @@
 package models
 
+import java.net.URL
 import java.util.Date
 
 /**
- * Status of extraction job.
- *
- * @author Luigi Marini
- *
+ * Status information about extractors and extractions.
  */
 case class Extraction(
   id: UUID = UUID.generate,
@@ -16,28 +14,27 @@ case class Extraction(
   start: Option[Date],
   end: Option[Date])
 
- /**
-  *  Currently running extractor name
-  */ 
-
+/**
+ * Currently running extractor name
+ */
 case class ExtractorNames(
-   name:String=""    
+  name: String = ""
 )
 
 /**
  * An input type supported by an extractor
  */
-case class ExtractorInputType (
-inputType:String=""    
+case class ExtractorInputType(
+  inputType: String = ""
 )
 
 /**
  * Servers information running different extractors
  * and supported file formats
- * 
+ *
  */
 case class ExtractorServer(
-server: String="N/A"
+  server: String = "N/A"
 )
 
 /**
@@ -46,9 +43,50 @@ server: String="N/A"
  * This class may be omitted once the design and implementation for BD-289 are done
  */
 case class ExtractorDetail(
-    ip: String = "",
-    name: String = "",
-    var count: Int = 0
+  ip: String = "",
+  name: String = "",
+  var count: Int = 0
 )
+
+/**
+ * Information about individual extractors. An extractor should set this the first time it starts up.
+ *
+ * Modelled after node.js package.json
+ *
+ * @param id id internal to the system
+ * @param name lower case, no spaces, can use dashes
+ * @param version the version, for example 1.3.5
+ * @param updated date when this information was last updated
+ * @param description short description of what the extractor does
+ * @param author First Last <username@somedomain.org>
+ * @param contributors list of contributors with same format as author
+ * @param contexts the ids of the contexts defining the metadata uploaded by the extractors
+ * @param repository source code repository
+ * @param external_services external services used by the extractor
+ * @param libraries libraries on which the code depends
+ * @param bibtex bibtext formatted citation of relevant papers
+ */
+case class ExtractionInfo(
+  id: UUID,
+  name: String,
+  version: String,
+  updated: Date,
+  description: String,
+  author: String,
+  contributors: List[String],
+  contexts: List[UUID],
+  repository: Repository,
+  external_services: List[URL],
+  libraries: List[String],
+  bibtex: List[String]
+)
+
+/**
+ * Source code repository
+ *
+ * @param repType git, hg, svn
+ * @param repUrl the url of the repository, for example https://opensource.ncsa.illinois.edu/stash/scm/cats/clowder.git
+ */
+case class Repository(repType: String, repUrl: String)
 
 
