d48bd2a529e6cbbcb697589f2fc5c10d21307ea1
Luigi Marini <lmarini@illinois.edu>
2015-07-21 11:09:55 -0500

    Simple JSON-LD metadata pages for datasets and files.

---
+++ app/controllers/Metadata.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import javax.inject.Inject
+
+import api.{WithPermission, Permission}
+import models.{ResourceRef, UUID}
+import services._
+
+/**
+ * View JSON-LD metadata for all resources.
+ */
+class Metadata @Inject() (
+  files: FileService,
+  datasets: DatasetService,
+  metadata: MetadataService) extends SecuredController {
+
+  def view(id: UUID) = SecuredAction(authorization = WithPermission(Permission.ShowFilesMetadata)) { implicit request =>
+    metadata.getMetadataById(id) match {
+      case Some(m) => Ok(views.html.metadatald.view(List(m)))
+      case None => NotFound
+    }
+  }
+
+  def file(file_id: UUID) = SecuredAction(authorization = WithPermission(Permission.ShowFilesMetadata)) { implicit request =>
+    files.get(file_id) match {
+      case Some(file) => {
+        val m = metadata.getMetadataByAttachTo(ResourceRef(ResourceRef.file, file_id))
+        Ok(views.html.metadatald.viewFile(file, m))
+      }
+      case None => NotFound
+    }
+  }
+
+  def dataset(dataset_id: UUID) = SecuredAction(authorization = WithPermission(Permission.ShowFilesMetadata)) { implicit request =>
+    datasets.get(dataset_id) match {
+      case Some(dataset) => {
+        val m = metadata.getMetadataByAttachTo(ResourceRef(ResourceRef.dataset, dataset_id))
+        Ok(views.html.metadatald.viewDataset(dataset, m))
+      }
+      case None => NotFound
+    }
+  }
+}
