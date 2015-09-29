4e5660003eaf7e277b64d00259982f2d41792a21
Brock Angelo <jba@illinois.edu>
2015-09-29 11:35:13 -0500

    update relatedSensors so we can assign it to any type of object

--- app/assets/javascripts/sensors/assign.js
+++ app/assets/javascripts/sensors/assign.js
@@ -1,4 +1,4 @@
-function associateWithSensor(dataset_id, dashboard_url) {
+function associateWithSensor(resource_type, item_id, dashboard_url) {
 
     var request = jsRoutes.api.Geostreams.searchSensors().ajax({
         type: 'GET',
@@ -7,7 +7,7 @@ function associateWithSensor(dataset_id, dashboard_url) {
     });
 
     request.done(function (response, textStatus, jqXHR){
-        showModal(response, dataset_id, dashboard_url);
+        showModal(response, resource_type, item_id, dashboard_url);
     });
 
     request.fail(function (jqXHR, textStatus, errorThrown){
@@ -17,9 +17,9 @@ function associateWithSensor(dataset_id, dashboard_url) {
     return false;
 }
 
-function showModal(sensors, dataset_id, dashboard_url) {
+function showModal(sensors, resource_type, item_id, dashboard_url) {
     var modalTemplate = Handlebars.getTemplate('../assets/templates/sensors/assign'); // TODO make it portable to other contexts
-    var html = modalTemplate({resource_type : "Dataset", sensors: sensors});
+    var html = modalTemplate({resource_type : resource_type, sensors: sensors});
     $('.container').append(html);
     $('#sensors-assign').modal();
 
@@ -33,26 +33,26 @@ function showModal(sensors, dataset_id, dashboard_url) {
     $('#sensors_add').unbind().click(function() {
         var sensor_id = $('.list-group-item.active').data("sensor-id");
         var sensor_name = $('.list-group-item.active').data("sensor-name");
-        console.log('Associating dataset ' + dataset_id + ' to sensor ' + sensor_id + " / " + sensor_name);
-        associateDatasetWithSensor(dataset_id, sensor_id, sensor_name, dashboard_url);
+        console.log('Associating ' + resource_type + ': ' + item_id + ' to sensor ' + sensor_id + " / " + sensor_name);
+        associateItemWithSensor(resource_type, item_id, sensor_id, sensor_name, dashboard_url);
     });
 
     return false;
 }
 
-function associateDatasetWithSensor(dataset_id, sensor_id, sensor_name, dashboard_url) {
+function associateItemWithSensor(resource_type, item_id, sensor_id, sensor_name, dashboard_url) {
 
     var request = jsRoutes.api.Relations.add().ajax({
         type: 'POST',
         data: JSON.stringify(
-            {"source": {"id":dataset_id,"resourceType":"dataset"},
+            {"source": {"id":item_id,"resourceType":resource_type.toLowerCase()},
                 "target": {"id":sensor_id.toString(),"resourceType":"sensor"}}),
         dataType: "json",
         contentType: "application/json; charset=utf-8"
     });
 
     request.done(function (response, textStatus, jqXHR){
-        console.log('Done associating dataset ' + dataset_id + ' to sensor ' + sensor_id + " / " + sensor_name);
+        console.log('Done associating ' + resource_type + ' ' + item_id + ' to sensor ' + sensor_id + " / " + sensor_name);
 
         // append new element to list
         $('#sensors-list').append('<li><a href="' + dashboard_url + sensor_name + '/">' +sensor_name+'</a></li>');
