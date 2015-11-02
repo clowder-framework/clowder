434e5141050b11318f01edf25922346edee8478d
Yan <yanzhao3@illinois.edu>
2015-11-02 13:51:56 -0600

    use identify instead of option[identify] for collection.author

--- app/controllers/Collections.scala
+++ app/controllers/Collections.scala
@@ -210,16 +210,16 @@ class Collections @Inject()(datasets: DatasetService, collections: CollectionSer
 
           var collection : Collection = null
           if (colSpace(0) == "default") {
-              collection = Collection(name = colName(0), description = colDesc(0), created = new Date, author = null)
+              collection = Collection(name = colName(0), description = colDesc(0), created = new Date, author = identity)
           }
           else {
             val stringSpaces = colSpace(0).split(",").toList
             val colSpaces: List[UUID] = stringSpaces.map(aSpace => if(aSpace != "") UUID(aSpace) else None).filter(_ != None).asInstanceOf[List[UUID]]
-            collection = Collection(name = colName(0), description = colDesc(0), created = new Date, author = null, spaces = colSpaces)
+            collection = Collection(name = colName(0), description = colDesc(0), created = new Date, author = identity, spaces = colSpaces)
           }
 
           Logger.debug("Saving collection " + collection.name)
-          collections.insert(Collection(id = collection.id, name = collection.name, description = collection.description, created = collection.created, author = Some(identity), spaces = collection.spaces))
+          collections.insert(Collection(id = collection.id, name = collection.name, description = collection.description, created = collection.created, author = collection.author, spaces = collection.spaces))
 
           //index collection
             val dateFormat = new SimpleDateFormat("dd/MM/yyyy")
@@ -346,7 +346,7 @@ class Collections @Inject()(datasets: DatasetService, collections: CollectionSer
         for(k <- userListSpaceRoleTupleMap.keys) userListSpaceRoleTupleMap += ( k -> userListSpaceRoleTupleMap(k).distinct.sortBy(_._1.toLowerCase) )
 
         if(userList.nonEmpty) {
-          val currUserIsAuthor = user.get.identityId.userId.equals(collection.author.get.identityId.userId)
+          val currUserIsAuthor = user.get.identityId.userId.equals(collection.author.identityId.userId)
           Ok(views.html.collections.users(collection, userListSpaceRoleTupleMap, currUserIsAuthor, userList))
         }
         else Redirect(routes.Collections.collection(id)).flashing("error" -> s"Error: No users found for collection $id.")
--- app/services/mongodb/MongoDBCollectionService.scala
+++ app/services/mongodb/MongoDBCollectionService.scala
@@ -266,13 +266,13 @@ class MongoDBCollectionService @Inject() (datasets: DatasetService, userService:
     val order = MongoDBObject("created"-> -1)
     if (date == "") {
       var collectionList = Collection.findAll.sort(order).limit(limit).toList
-      collectionList= collectionList.filter(x=> x.author.get.email.toString == "Some(" +email +")")
+      collectionList= collectionList.filter(x=> x.author.email.toString == "Some(" +email +")")
       collectionList
     } else {
       val sinceDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(date)
       Logger.info("After " + sinceDate)
       var collectionList = Collection.find("created" $lt sinceDate).sort(order).limit(limit).toList
-      collectionList= collectionList.filter(x=> x.author.get.email.toString == "Some(" +email +")")
+      collectionList= collectionList.filter(x=> x.author.email.toString == "Some(" +email +")")
       collectionList
     }
   }
@@ -284,7 +284,7 @@ class MongoDBCollectionService @Inject() (datasets: DatasetService, userService:
     var order = MongoDBObject("created"-> -1)
     if (date == "") {
       var collectionList = Collection.findAll.sort(order).limit(limit).toList
-      collectionList= collectionList.filter(x=> x.author.get.email.toString == "Some(" +email +")")
+      collectionList= collectionList.filter(x=> x.author.email.toString == "Some(" +email +")")
       collectionList
     } else {
       order = MongoDBObject("created"-> 1)
@@ -292,7 +292,7 @@ class MongoDBCollectionService @Inject() (datasets: DatasetService, userService:
       Logger.info("Before " + sinceDate)
       var collectionList = Collection.find("created" $gt sinceDate).sort(order).limit(limit + 1).toList.reverse
       collectionList = collectionList.filter(_ != collectionList.last)
-      collectionList= collectionList.filter(x=> x.author.get.email.toString == "Some(" +email +")")
+      collectionList= collectionList.filter(x=> x.author.email.toString == "Some(" +email +")")
       collectionList
     }
   }
