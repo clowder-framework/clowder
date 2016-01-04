888282fc7d2dda9dd7f11802bacf58b2c9ed9524
Todd Nicholson <tcnichol@illinois.edu>
2016-01-04 15:19:04 -0600

    initial changes to add nested collections to latest dev branch

--- app/models/Collection.scala
+++ app/models/Collection.scala
@@ -16,7 +16,11 @@ case class Collection(
   spaces: List[UUID] = List.empty,
   lastModifiedDate: Date = new Date(),
   followers: List[UUID] = List.empty,
+  parent_collection_ids : List[String] = List.empty,
+  child_collection_ids : List[String] = List.empty,
+  root_flag : Boolean = false,
   metadataCount: Long = 0,
+  childCollectionsCount: Option[Integer] = None,
   @deprecated("use Metadata","since the use of jsonld") jsonldMetadata : List[Metadata]= List.empty)
 
 object Collection {
