c94e611136eb0bcd11e132d9d8bad9d2629ff62a
Brock Angelo <jba@illinois.edu>
2016-01-26 14:16:45 -0600

    cleanup of create and edit forms

--- public/javascripts/geostreams/sensorCreate.js
+++ public/javascripts/geostreams/sensorCreate.js
@@ -104,40 +104,6 @@ $(document).ready(function() {
     $("#instrument-" + instrumentNumber).remove();
   });
 
-
-  // TODO store this in the sensor config
-  sensorType.on('change', function() {
-    var sensorType = $(this).val();
-    var hasDepth = $("#hasDepth");
-    var sensorTypeSensorCount = $("#sensorTypeSensorCount");
-    var sensorTypeMultipleInstruments = $("#sensorTypeMultipleInstruments");
-    var instrumentContents1 = $("#instrument-contents-1");
-    var instrumentLink1 = $("#instrument-link-1");
-    var addInstrument = $("#addInstrument");
-
-    $("#sensorTypeSummary").text(sensorType);
-    switch(sensorType) {
-      case "5":
-      case "6":
-      case "7":
-        hasDepth.show();
-        sensorTypeSensorCount.text('multiple');
-        sensorTypeMultipleInstruments.text('s');
-        instrumentContents1.collapse('hide');
-        instrumentLink1.text('Instrument #1 Information');
-        addInstrument.show();
-        break;
-      default:
-        hasDepth.hide();
-        sensorTypeSensorCount.text('1');
-        sensorTypeMultipleInstruments.text('');
-        instrumentContents1.collapse('show');
-        instrumentLink1.text('Instrument Information');
-        addInstrument.hide();
-        break;
-    }
-  });
-
   // enable tooltips
   $('[data-toggle="tooltip"]').tooltip();
 
