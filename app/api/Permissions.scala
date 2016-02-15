404fee28fb426fb07a9eb19b9724db24e2144759
Indira Gutierrez <indiragp@illinois.edu>
2016-02-15 11:42:47 -0600

    Removing follow button from folder. Fixing permissions for files within folders.

--- app/api/Permissions.scala
+++ app/api/Permissions.scala
@@ -93,6 +93,7 @@ object Permission extends Enumeration {
   lazy val collections: CollectionService = DI.injector.getInstance(classOf[CollectionService])
   lazy val datasets: DatasetService = DI.injector.getInstance(classOf[DatasetService])
   lazy val spaces: SpaceService = DI.injector.getInstance(classOf[SpaceService])
+  lazy val folders: FolderService = DI.injector.getInstance(classOf[FolderService])
   lazy val users: services.UserService = DI.injector.getInstance(classOf[services.UserService])
   lazy val comments: services.CommentService = DI.injector.getInstance(classOf[services.CommentService])
   lazy val curations: services.CurationService = DI.injector.getInstance(classOf[services.CurationService])
@@ -228,6 +229,16 @@ object Permission extends Enumeration {
               spaceId => for(role <- users.getUserRoleInSpace(clowderUser.id, spaceId)) {
                 if(role.permissions.contains(permission.toString))
                   return true
+              }
+            }
+          }
+          folders.findByFileId(id).foreach { folder =>
+            datasets.get(folder.parentDatasetId).foreach { dataset =>
+              dataset.spaces.map {
+                spaceId => for(role <- users.getUserRoleInSpace(clowderUser.id, spaceId)) {
+                  if(role.permissions.contains(permission.toString))
+                    return true
+                }
               }
             }
           }
