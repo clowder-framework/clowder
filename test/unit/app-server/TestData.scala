81dcc52f93038855127ab7975d01608b12583ce0
Indira Gutierrez <indiragp@illinois.edu>
2016-03-08 12:55:12 -0600

    Trying to fix tests.

--- test/unit/app-server/TestData.scala
+++ test/unit/app-server/TestData.scala
@@ -5,11 +5,11 @@ import models.{Tag, UUID, File, Metadata, UserAgent}
 import securesocial.core.{AuthenticationMethod, IdentityId, SocialUser}
 import java.net.URL
 import play.api.libs.json.JsObject
+import models.MiniUser
 
 /**
  * Mixin of default data to use for testing.
  *
- * @author Luigi Marini
  */
 trait TestData {
   var testUser = SocialUser(identityId = IdentityId("john@doe.com","userpass"),
@@ -19,8 +19,15 @@ trait TestData {
     email = Some("john@doe.com"),
     avatarUrl = None,
     authMethod = AuthenticationMethod.UserPassword)
+
+  var testMiniUser = MiniUser(
+    id = UUID("56d8afec7d840aec3068a334"),
+    fullName = "John Doe",
+    email = Some("john@doe.com"),
+    avatarURL = ""
+    )
   
-  var testFile = File(id = UUID.generate, filename = "foo.txt", author = testUser, uploadDate =  new Date, contentType = "text/plain")
+  var testFile = File(id = UUID.generate, filename = "foo.txt", author = testMiniUser, uploadDate =  new Date, contentType = "text/plain")
 
   var testTag = Tag(UUID.generate, "foo", None, None, new Date)
     
