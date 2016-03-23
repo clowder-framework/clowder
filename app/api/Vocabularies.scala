e4745f8d9696ec1d8112a64a2e36513622df4b53
Todd Nicholson <tcnichol@illinois.edu>
2016-03-22 19:11:03 -0500

    added create from json method for vocabulary

--- app/api/Vocabularies.scala
+++ app/api/Vocabularies.scala
@@ -85,11 +85,34 @@ class Vocabularies @Inject() (vocabularyService: VocabularyService, userService 
     Ok(toJson(vocabs))
   }
 
-  @ApiOperation(value = "Create a vocabulary",
+  @ApiOperation(value = "Create a template",
     notes = "",
     responseClass = "None", httpMethod = "POST")
-  def createVocabulary() = PermissionAction(Permission.CreateVocabulary){ implicit request =>
-    Ok(toJson("not implemented"))
+  def createVocabularyFromJson() = AuthenticatedAction (parse.json) { implicit request =>
+    val user = request.user
+    var t : Vocabulary = null
+    user match {
+      case Some(identity) => {
+        (request.body \ "keys").asOpt[String] match {
+          case Some(keys) => {
+            val name = (request.body\"name").asOpt[String].getOrElse("")
+            val description = (request.body \ "description").asOpt[String].getOrElse("")
+            t = Vocabulary(author = Some(identity), created = new Date(),name = name,keys = keys.split(",").toList , description = description.split(',').toList)
+
+            vocabularyService.insert(t) match {
+              case Some(id) => {
+                Ok(toJson(Map("id" -> id)))
+              }
+              case None => BadRequest("could not create vocbabulry")
+            }
+
+          }
+          case None => BadRequest("no keys supplied")
+        }
+
+      }
+      case None => BadRequest("no user supplied")
+    }
   }
 
 
