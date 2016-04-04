51b1b18d0e45397f43a7dd8e92e51df5d5748cb4
Yan Zhao <yanzhao3@illinois.edu>
2016-04-04 08:38:21 -0500

    Update app/controllers/Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -51,7 +51,8 @@ class Application @Inject() (files: FileService, collections: CollectionService,
         Redirect(routes.Error.notActivated())
       }
       case Some(clowderUser) if clowderUser.active => {
-        newsfeedEvents= newsfeedEvents ::: events.getEventsByUser(clowderUser, Some(20))
+        newsfeedEvents = (newsfeedEvents ::: events.getEventsByUser(clowderUser, Some(20)))
+          .sorted(Ordering.by((_: Event).created).reverse).take(20)
         val datasetsUser = datasets.listUser(4, Some(clowderUser), request.superAdmin, clowderUser)
         val datasetcommentMap = datasetsUser.map { dataset =>
           var allComments = comments.findCommentsByDatasetId(dataset.id)
