c8b0ad62b6eb107090ec9d232f0879b1224d00ad
Todd Nicholson <tcnichol@illinois.edu>
2016-04-27 14:42:59 -0500

    fixed typo, removed unused method

--- app/services/mongodb/MongoDBVocabularyService.scala
+++ app/services/mongodb/MongoDBVocabularyService.scala
@@ -78,10 +78,6 @@ class MongoDBVocabularyService @Inject() (userService: UserService) extends Voca
     Vocabulary.findAll.toList.filter((v: Vocabulary )=> (!v.description.intersect(desc).isEmpty))
   }
 
-  private def list() : List[Vocabulary] = {
-    var vocabularies = List.empty[Vocabulary]
-  }
-
 }
 
 object Vocabulary extends ModelCompanion[Vocabulary, ObjectId] {
