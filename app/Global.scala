db62e04aeb630840159f69e144c8010bb99cb848
Yan <yanzhao3@illinois.edu>
2016-05-08 20:14:22 -0500

    enableRefererAsOriginalUrl

--- app/Global.scala
+++ app/Global.scala
@@ -3,7 +3,6 @@ import play.api.{GlobalSettings, Application}
 import play.api.Logger
 import play.filters.gzip.GzipFilter
 import play.libs.Akka
-import securesocial.core.SecureSocial
 import services.{UserService, DI, AppConfiguration}
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -78,13 +77,9 @@ object Global extends WithFilters(new GzipFilter(), new Jsonp(), CORSFilter()) w
     val sw = new StringWriter()
     val pw = new PrintWriter(sw)
     ex.printStackTrace(pw)
-    Logger.debug(SecureSocial.currentUser(request).get.getClass.toString)
-    val users: UserService =  DI.injector.getInstance(classOf[UserService])
-
-    implicit val user = users.findByIdentity(SecureSocial.currentUser(request).get)
-
     Future(InternalServerError(
-      views.html.errorPage(request, sw.toString.replace("\n", "   "))(user)))
+      views.html.errorPage(request, sw.toString.replace("\n", "   "))
+    ))
   }
 
   override def onHandlerNotFound(request: RequestHeader) = {
