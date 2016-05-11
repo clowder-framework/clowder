3d75f20183ab5b9a272ef60053a5995ad5276ceb
Smruti Padhy <spadhy@illinois.edu>
2016-05-11 11:38:27 -0500

    delete file api call corrected

--- scripts/web/tests/tests.py
+++ scripts/web/tests/tests.py
@@ -347,7 +347,7 @@ def extract(host, port, key, file, wait):
 	metadata = requests.get('http://' + host + ':' + port + '/api/extractions/' + file_id + '/metadata?key=' + key).json()
 	metadata["technicalmetadata"] = requests.get('http://' + host + ':' + port + '/api/files/' + file_id + '/technicalmetadatajson?key=' + key).json()
 	metadata = json.dumps(metadata)
-    delete_status = requests.delete('http://' + host + ':' + port + '/api/files/' + file_id + key).json()
+    requests.delete('http://' + host + ':' + port + '/api/files/' + file_id +'?key='+ key)
     
 	return metadata
 
