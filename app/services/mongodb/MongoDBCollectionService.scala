8ed10553cbf1065a88f7d7db5bb11d591c78161a
Todd Nicholson <tcnichol@illinois.edu>
2016-05-15 14:02:28 -0500

    removed changes

--- app/services/mongodb/MongoDBCollectionService.scala
+++ app/services/mongodb/MongoDBCollectionService.scala
@@ -408,22 +408,6 @@ class MongoDBCollectionService @Inject() (
               datasets.addCollection(dataset.id, collection.id)
               datasets.index(dataset.id)
               index(collection.id)
-              //add collection spaces to dataset
-              //TODO - todd n
-              for (spaceId <- collection.spaces){
-                //TODO check if the dataset is alread in the space
-                spaceService.get(spaceId) match {
-                  case Some(space) => {
-                    spaceService.addDataset(datasetId, spaceId)
-                  }
-                  case None => {
-                    Logger.debug("No space found for space " + spaceId)
-                  }
-                }
-
-
-              }
-
 
               if(collection.thumbnail_id.isEmpty && !dataset.thumbnail_id.isEmpty){
                   Collection.dao.collection.update(MongoDBObject("_id" -> new ObjectId(collection.id.stringify)),
