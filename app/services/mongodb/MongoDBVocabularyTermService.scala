0776e16335b19ab484728a72fd14820bfed2d3b1
Todd Nicholson <tcnichol@illinois.edu>
2016-06-03 17:10:01 -0500

    added mongodbserviceimpl for vocabularyterm

---
+++ app/services/mongodb/MongoDBVocabularyTermService.scala
@@ -0,0 +1,57 @@
+package services.mongodb
+
+import com.mongodb.casbah.Imports._
+import securesocial.core.Identity
+import services.mongodb.MongoContext.context
+import com.mongodb.casbah.commons.MongoDBObject
+
+import com.novus.salat.dao.{SalatMongoCursor, ModelCompanion, SalatDAO}
+import org.bson.types.ObjectId
+import models._
+import javax.inject.{Singleton, Inject}
+
+
+import com.mongodb.casbah.WriteConcern
+import services.{VocabularyService, UserService}
+import play.api.Play._
+import scala.util.{Success, Try}
+
+/**
+  * Created by todd_n on 2/9/16.
+  */
+@Singleton
+class MongoDBVocabularyService @Inject() (userService: UserService) extends VocabularyService {
+
+  def count(): Long = {
+    Vocabulary.count(MongoDBObject())
+  }
+
+  def insert(vocabulary: Vocabulary): Option[String] = {
+    Vocabulary.insert(vocabulary).map(_.toString)
+  }
+
+  def listAll(): List[Vocabulary] = {
+    Vocabulary.findAll().toList
+  }
+
+  def get(id: UUID): Option[Vocabulary] = {
+    Vocabulary.findOneById(new ObjectId(id.stringify))
+  }
+
+  def delete(id: UUID) = Try {
+    Vocabulary.findOneById(new ObjectId(id.stringify)) match {
+      case Some(vocab) => {
+        Vocabulary.remove(MongoDBObject("_id" -> new ObjectId(vocab.id.stringify)))
+        Success
+      }
+      case None => Success
+    }
+  }
+}
+
+object VocabularyTerm extends ModelCompanion[VocabularyTerm, ObjectId] {
+  val dao = current.plugin[MongoSalatPlugin] match {
+    case None => throw new RuntimeException("No MongoSalatPlugin");
+    case Some(x) => new SalatDAO[Vocabulary, ObjectId](collection = x.collection("vocabularyterms")) {}
+  }
+}
