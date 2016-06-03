27ce2e96ff3380f805817b6132b2001c5c9556a8
Todd Nicholson <tcnichol@illinois.edu>
2016-06-03 13:28:04 -0500

    added new class vocabularyTerm
    The purpose is so that a key can be stored with units (option) or default value.
    
    Example, let's say there is a Vocabulary which represents some kind of manufacturing process. The process will have Terms such as 'material' and a VocabularyTerm such as 'temperature' could contain default units (grad, kelvin) or a default value.

---
+++ app/models/VocabularyTerm.scala
@@ -0,0 +1,30 @@
+package models
+
+import java.util.Date
+
+import play.api.libs.json.{Json, JsValue, Writes}
+import securesocial.core.Identity
+
+/**
+  * Created by todd_n on 2/8/16.
+  */
+case class VocabularyTerm(
+  id : UUID = UUID.generate(),
+  author : Option[Identity],
+  created : Date = new Date(),
+  key : String,
+  units : Option[String],
+  default_value : Option[String]
+  )
+
+
+object VocabularyTerm{
+  implicit val vocabularyTermWrites = new Writes[VocabularyTerm] {
+    def writes(vocabularyTerm : VocabularyTerm) : JsValue = {
+      Json.obj("id" -> vocabularyTerm.id.toString,"key"->vocabularyTerm.key,"default_value"->vocabularyTerm.default_value.getOrElse(""))
+    }
+  }
+}
+
+
+
