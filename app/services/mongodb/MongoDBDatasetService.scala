dd4eed7f9495a4fc52ad325adf2887bf494dbf55
Yan <yanzhao3@illinois.edu>
2016-06-06 21:53:32 -0500

    update filterAccess for dataset

--- app/services/mongodb/MongoDBDatasetService.scala
+++ app/services/mongodb/MongoDBDatasetService.scala
@@ -222,9 +222,8 @@ class MongoDBDatasetService @Inject() (
           if (permissions.contains(Permission.ViewDataset)) {
             orlist += MongoDBObject("public" -> true)
           }
-          if (user == owner) {
-            orlist += MongoDBObject("spaces" -> List.empty)
-          }
+          orlist += MongoDBObject("author._id" -> new ObjectId(u.id.stringify))
+
           val permissionsString = permissions.map(_.toString)
           val okspaces = u.spaceandrole.filter(_.role.permissions.intersect(permissionsString).nonEmpty)
           if (okspaces.nonEmpty) {
