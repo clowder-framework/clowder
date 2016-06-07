e1fdd80e68ab044d3a0c401d400406f9c156039f
Yan <yanzhao3@illinois.edu>
2016-06-07 08:52:24 -0500

    collection filterAccess

--- app/services/mongodb/MongoDBCollectionService.scala
+++ app/services/mongodb/MongoDBCollectionService.scala
@@ -181,7 +181,7 @@ class MongoDBCollectionService @Inject() (
           if (permissions.contains(Permission.ViewCollection)) {
             orlist += MongoDBObject("public" -> true)
           }
-          orlist += MongoDBObject("spaces" -> List.empty) ++ MongoDBObject("author._id" -> new ObjectId(u.id.stringify))
+          orlist +=  MongoDBObject("author._id" -> new ObjectId(u.id.stringify))
           val permissionsString = permissions.map(_.toString)
           val okspaces = u.spaceandrole.filter(_.role.permissions.intersect(permissionsString).nonEmpty)
           if (okspaces.nonEmpty) {
