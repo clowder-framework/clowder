f8600dc12189b3bdea5244471907b6c8cfa97dc7
Rob Kooper <kooper@illinois.edu>
2016-07-28 10:23:07 -0500

    add patch version

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ import NativePackagerKeys._
 object ApplicationBuild extends Build {
 
   val appName = "clowder"
-  val version = "0.9.4"
+  val version = "0.9.4.1"
   val jvm = "1.7"
 
   def appVersion: String = {
