6a9da363c62edc05a40f992fd0eef68ce1808661
Max Burnette <mburnet88@gmail.com>
2016-08-16 09:27:21 -0500

    Add support for manual extraction on datasets

--- app/controllers/Extractors.scala
+++ app/controllers/Extractors.scala
@@ -5,14 +5,14 @@ import play.api.mvc.Controller
 import api.Permission
 import javax.inject.{Inject, Singleton}
 
-import services.{ExtractionService, ExtractorService, FileService}
+import services.{ExtractionService, ExtractorService, FileService, DatasetService}
 
 /**
  * Information about extractors.
  */
 @Singleton
 class Extractors  @Inject() (extractions: ExtractionService,
-  extractorService: ExtractorService, fileService: FileService) extends Controller with SecuredController {
+  extractorService: ExtractorService, fileService: FileService, datasetService: DatasetService) extends Controller with SecuredController {
 
   def listAllExtractions = ServerAdminAction { implicit request =>
     implicit val user = request.user
@@ -20,11 +20,19 @@ class Extractors  @Inject() (extractions: ExtractionService,
     Ok(views.html.listAllExtractions(allExtractions))
   }
 
-  def submitExtraction(file_id: UUID) = PermissionAction(Permission.EditFile, Some(ResourceRef(ResourceRef.file, file_id))) { implicit request =>
+  def submitFileExtraction(file_id: UUID) = PermissionAction(Permission.EditFile, Some(ResourceRef(ResourceRef.file, file_id))) { implicit request =>
     val extractors = extractorService.listExtractorsInfo()
     fileService.get(file_id) match {
-      case Some(file) => Ok(views.html.extractions.submitExtraction(extractors, file))
+      case Some(file) => Ok(views.html.extractions.submitFileExtraction(extractors, file))
       case None => InternalServerError("File not found")
+    }
+  }
+
+  def submitDatasetExtraction(ds_id: UUID) = PermissionAction(Permission.EditDataset, Some(ResourceRef(ResourceRef.dataset, ds_id))) { implicit request =>
+    val extractors = extractorService.listExtractorsInfo()
+    datasetService.get(ds_id) match {
+      case Some(ds) => Ok(views.html.extractions.submitDatasetExtraction(extractors, ds))
+      case None => InternalServerError("Dataset not found")
     }
   }
 }
