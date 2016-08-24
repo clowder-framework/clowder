97ea431f1ccfc3810dc7003728b5f7adadb9dd0d
mo <ondrejce@illinois.edu>
2016-08-24 13:08:31 -0500

    change endpoint name, delete comment

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -8,7 +8,7 @@ import securesocial.core.{UserService, SecureSocial}
   * Login class for checking if User is still logged through the securesocial.controllers.
   */
 class Login extends SecuredController {
-  def isLogged() = Action { implicit request =>
+  def isLoggedIn() = Action { implicit request =>
     val result = for (
       authenticator <- SecureSocial.authenticatorFromRequest(request);
       identity <- UserService.find(authenticator.identityId)
@@ -16,7 +16,6 @@ class Login extends SecuredController {
       // we should be able to use the authenticator.timedOut directly but it never returns true
       identity
     }
-    //Logger.debug("User's identity: " + result)
 
     result match {
       case Some(a) => Ok("yes")
