28243eec2f1edc4afa6804d9951abfcf7e78df75
Max Burnette <mburnet88@gmail.com>
2016-09-19 13:26:31 -0500

    Squashed commit of the following:
    commit c272b76df5465ab0938bbb47604a61cbe71535ce
    Author: Max Burnette <mburnet88@gmail.com>
    Date:   Thu Sep 8 09:38:30 2016 -0500
    
        Only return full path if server admin
    
    commit c1f2776ebbe84a066e233fc69451af7e8228a639
    Author: Max Burnette <mburnet88@gmail.com>
    Date:   Mon Aug 29 13:44:53 2016 -0500
    
        Don't send file path for non-disk storage
    
    commit 8c4d877848eb49e0c1c23dd2a27a0cd16bd3f17e
    Author: Max Burnette <mburnet88@gmail.com>
    Date:   Thu Aug 18 09:47:42 2016 -0500
    
        Also include path on dataset file list
    
    commit 46176b47762db775f1771515edae2947b10ed43a
    Author: Max Burnette <mburnet88@gmail.com>
    Date:   Thu Aug 18 09:26:56 2016 -0500
    
        Don't try to include path with mongo storage
    
    commit f5d6a01793899e6f4aa49d148bb7ce27faf72295
    Author: Max Burnette <mburnet88@gmail.com>
    Date:   Thu Aug 18 09:20:38 2016 -0500
    
        Return file path with file info from API
    
        /api/files/:id/metadata

--- app/api/Datasets.scala
+++ app/api/Datasets.scala
@@ -801,7 +801,13 @@ class  Datasets @Inject()(
     datasets.get(id) match {
       case Some(dataset) => {
         val list: List[JsValue]= dataset.files.map(fileId => files.get(fileId) match {
-          case Some(file) => jsonFile(file)
+          case Some(file) => {
+            val serveradmin = request.user match {
+              case Some(u) => u.serverAdmin
+              case None => false
+            }
+            jsonFile(file, serveradmin)
+          }
           case None => Logger.error(s"Error getting File $fileId")
         }).asInstanceOf[List[JsValue]]
         Ok(toJson(list))
@@ -817,10 +823,20 @@ class  Datasets @Inject()(
     datasets.get(id) match {
       case Some(dataset) => {
         val listFiles: List[JsValue]= dataset.files.map(fileId => files.get(fileId) match {
-          case Some(file) => jsonFile(file)
+          case Some(file) => {
+            val serveradmin = request.user match {
+              case Some(u) => u.serverAdmin
+              case None => false
+            }
+            jsonFile(file, serveradmin)
+          }
           case None => Logger.error(s"Error getting File $fileId")
         }).asInstanceOf[List[JsValue]]
-        val list = listFiles ++ getFilesWithinFolders(id)
+        val serveradmin = request.user match {
+          case Some(u) => u.serverAdmin
+          case None => false
+        }
+        val list = listFiles ++ getFilesWithinFolders(id, serveradmin)
         Ok(toJson(list))
       }
       case None => Logger.error("Error getting dataset" + id); InternalServerError
@@ -866,7 +882,7 @@ class  Datasets @Inject()(
   }
 
 
-  private def getFilesWithinFolders(id: UUID): List[JsValue] = {
+  private def getFilesWithinFolders(id: UUID, serveradmin: Boolean = false): List[JsValue] = {
     val output = new ListBuffer[JsValue]()
     datasets.get(id) match {
       case Some(dataset) => {
@@ -875,7 +891,7 @@ class  Datasets @Inject()(
           folder =>
             folder.files.map {
               fileId => files.get(fileId) match {
-                case Some(file) => output += jsonFile(file)
+                case Some(file) => output += jsonFile(file, serveradmin)
                 case None => Logger.error(s"Error getting file $fileId")
               }
             }
@@ -886,9 +902,31 @@ class  Datasets @Inject()(
     output.toList.asInstanceOf[List[JsValue]]
   }
 
-  def jsonFile(file: models.File): JsValue = {
-    toJson(Map("id" -> file.id.toString, "filename" -> file.filename, "contentType" -> file.contentType,
-      "date-created" -> file.uploadDate.toString(), "size" -> file.length.toString))
+  def jsonFile(file: models.File, serverAdmin: Boolean = false): JsValue = {
+    val defaultMap = Map(
+      "id" -> file.id.toString,
+      "filename" -> file.filename,
+      "contentType" -> file.contentType,
+      "date-created" -> file.uploadDate.toString(),
+      "size" -> file.length.toString)
+
+    // Only include filepath if using DiskByte storage and user is serverAdmin
+    val jsonMap = file.loader match {
+      case "services.filesystem.DiskByteStorageService" => {
+        if (serverAdmin)
+          Map(
+            "id" -> file.id.toString,
+            "filename" -> file.filename,
+            "filepath" -> file.loader_id,
+            "contentType" -> file.contentType,
+            "date-created" -> file.uploadDate.toString(),
+            "size" -> file.length.toString)
+        else
+          defaultMap
+      }
+      case _ => defaultMap
+    }
+    toJson(jsonMap)
   }
 
   //Update Dataset Information code starts
