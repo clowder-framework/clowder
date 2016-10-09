5993865a8d4a545cce97812e1fcff5b1bdc18664
mo <ondrejce@illinois.edu>
2016-10-09 16:00:44 -0500

    shows all public datasets independently of all datasets, new 'public' call instead of filtering the 'all datasets'

--- app/controllers/Spaces.scala
+++ app/controllers/Spaces.scala
@@ -151,6 +151,7 @@ class Spaces @Inject()(spaces: SpaceService, users: UserService, events: EventSe
 	        var creatorActual: User = null
 	        val collectionsInSpace = spaces.getCollectionsInSpace(Some(id.stringify), Some(size))
 	        val datasetsInSpace = datasets.listSpace(size, id.toString(), user)
+          val publicDatasetsInSpace = datasets.listSpaceStatus(size, id.toString(), "publicAll", user)
 	        val usersInSpace = spaces.getUsersInSpace(id)
           var curationObjectsInSpace: List[CurationObject] = List()
 	        var inSpaceBuffer = usersInSpace.to[ArrayBuffer]
@@ -190,7 +191,7 @@ class Spaces @Inject()(spaces: SpaceService, users: UserService, events: EventSe
           if (play.api.Play.current.plugin[services.StagingAreaPlugin].isDefined) {
             curationObjectsInSpace = curationService.listSpace(Some(size),Some(id.stringify))
           }
-	        Ok(views.html.spaces.space(Utils.decodeSpaceElements(s), collectionsInSpace, datasetsInSpace, curationObjectsInSpace, userRoleMap))
+	        Ok(views.html.spaces.space(Utils.decodeSpaceElements(s), collectionsInSpace, publicDatasetsInSpace, datasetsInSpace, curationObjectsInSpace, userRoleMap))
       }
       case None => BadRequest(views.html.notFound(spaceTitle + " does not exist."))
     }
