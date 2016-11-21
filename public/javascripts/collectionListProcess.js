a430fb419561a565ad5a23b059f45d009d3e4050
Indira Gutierrez <indiragp@illinois.edu>
2016-11-21 16:41:42 -0600

    Renaming url to be redirected to in delete collection

--- public/javascripts/collectionListProcess.js
+++ public/javascripts/collectionListProcess.js
@@ -1,5 +1,5 @@
-function removeCollection(id, isreload, url, resourceFromType){
-	var url = jsRoutes.api.Collections.removeCollection(id)
+function removeCollection(id, isreload, newurl, resourceFromType){
+	var url = jsRoutes.api.Collections.removeCollection(id);
 	if(resourceFromType == 'space'){
 		var removeDatasets = $('#removedatasets').val();
 		url = jsRoutes.api.Collections.removeCollection(id , removeDatasets)
@@ -9,7 +9,7 @@ function removeCollection(id, isreload, url, resourceFromType){
 	});
 	request.done(function (response, textStatus, jqXHR){
 		if(isreload === "true")
-			window.location.href=url;
+			window.location.href=newurl;
 		else {
 			$('#'+ id+'-listitem').remove();
 			var obj = $('#'+ id+'-tile');
