fab9af029c83d4abc859e7354a3140ebd31b704d
Max Burnette <mburnet88@gmail.com>
2016-12-01 10:38:19 -0600

    Increment User count

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -14,18 +14,17 @@ import play.api.Play
 import securesocial.controllers.TemplatesPlugin
 import securesocial.core.providers.utils.Mailer
 import securesocial.core.providers.{Token, UsernamePasswordProvider}
-import services.AppConfiguration
+import services.{AppConfigurationService, AppConfiguration, UserService}
 import javax.inject.Inject
 
 import play.api.http.Status._
-import services.UserService
 import play.api.mvc.{Action, Results}
 import util.{Direction, Formatters, Mail}
 
 /**
  * Manage users.
  */
-class Users @Inject() (users: UserService) extends SecuredController {
+class Users @Inject() (users: UserService, appConfig: AppConfigurationService) extends SecuredController {
   //Custom signup initiation code, to be used if config is set to send signup link emails to admins to forward to users
 
   val TokenDurationKey = securesocial.controllers.Registration.TokenDurationKey
@@ -64,6 +63,7 @@ class Users @Inject() (users: UserService) extends SecuredController {
       isSignUp = isSignUp
     )
     securesocial.core.UserService.save(token)
+    appConfig.incrementCount("countof.users", 1)
     (uuid, token)
   }
 
