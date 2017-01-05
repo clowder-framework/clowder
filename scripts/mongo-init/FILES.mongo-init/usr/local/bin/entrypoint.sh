35777355cc7721110f171b426a6ebccbadc17b78
bing zhang <bing@illinois.edu>
2017-01-05 11:19:15 -0600

    rename the default user name and use MONGO_URI

--- scripts/mongo-init/FILES.mongo-init/usr/local/bin/entrypoint.sh
+++ scripts/mongo-init/FILES.mongo-init/usr/local/bin/entrypoint.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-if [ -z "$HOSTIP" ] | [ -z "$PASSWORD" ] | [ -z "$EMAIL_ADDRESS" ]; then
-        echo "parameters are empty (hostip, password, emailaddress)"
+if [ -z "$PASSWORD" ] | [ -z "$EMAIL_ADDRESS" ]; then
+        echo "parameters are empty (password, emailaddress)"
         exit
 fi
 
-hostip=$HOSTIP
-emailaddress=$EMAIL_ADDRESS
-
-if [ -z "$ADMIN" ]; then
-        admin=false
-else
-        admin=$ADMIN
+if [ "$MONGO_URI" == "" ]; then
+    if [ -n "$MONGO_PORT_27017_TCP_PORT" ]; then
+        MONGO_URI="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/clowder"
+    else
+        MONGO_URI="mongodb://127.0.0.1:27017/clowder"
+    fi
 fi
 
-if [ -z "$FIRST_NAME" ]; then
-        firstname="firstname"
-else
+emailaddress=$EMAIL_ADDRESS
+
+firstname="Normal"
+admin=false
+ADMIN=$(echo $ADMIN | tr '[:upper:]' '[:lower:]')
+if [[ $ADMIN == "true" ]]; then
+    admin=true
+    firstname="Admin"
+fi
+
+if [ ! -z "$FIRST_NAME" ]; then
         firstname=$FIRST_NAME
 fi
 
-if [ -z "$LAST_NAME" ]; then
-        lastname="lastname"
-else
+lastname="User"
+if [ ! -z "$LAST_NAME" ]; then
         lastname=$LAST_NAME
 fi
-
-fullname=$firstname$lastname
+IFS='%'
+fullname="$firstname $lastname"
 
 echo "+++++++++++++++++++++++++++++++++++++"
 echo "Add User Account into Clowder"
@@ -41,15 +47,15 @@ done
 
 echo "clowder is starting"
 
-query=$(mongo $HOSTIP:27017/clowder --eval "db.social.users.find({email: '"$emailaddress"', \"termsOfServices.accepted\": true}, {identityId:1})")
+query=$(mongo $MONGO_URI --eval "db.social.users.find({email: '"$emailaddress"', \"termsOfServices.accepted\": true}, {identityId:1})")
 
 if echo $query | grep -q "$emailaddress"; then
         echo "account found"
 else
         echo "account not found!"
 	hasher=$(python3 /usr/local/bin/passwd)
-	mongo $HOSTIP:27017/clowder --eval "db.social.users.remove({email: '"$emailaddress"'})"
-        mongo $HOSTIP:27017/clowder --eval "db.social.users.insert(
+	mongo $MONGO_URI --eval "db.social.users.remove({email: '"$emailaddress"'})"
+        mongo $MONGO_URI --eval "db.social.users.insert(
         {
                 identityId: {
 			userId: '"$emailaddress"', 
@@ -70,11 +76,13 @@ else
         	        password: '"$hasher"'
 		},
                 active: true, 
-                serverAdmin: false, 
+                serverAdmin: $admin, 
                 termsOfServices: {
 			accepted: true, 
 			acceptedDate: ISODate(\"2020-10-25T20:17:02.591Z\"), 
 			acceptedVersion: \"2016-06-06\"}
         });db.fsyncLock();db.fsyncUnlock();"
+
+unset IFS
 fi
 
