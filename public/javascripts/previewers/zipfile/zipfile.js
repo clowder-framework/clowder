8577a953da41d101903742ec4b27fa7acff033a4
Mike Lambert <lambert8@illinois.edu>
2018-10-25 14:58:31 -0500

    Included a pattern for loading preview data asynchronously

--- public/javascripts/previewers/zipfile/zipfile.js
+++ public/javascripts/previewers/zipfile/zipfile.js
@@ -9,33 +9,52 @@
 
     zip.workerScriptsPath = Configuration.previewer + '../../../';
 
-    $(Configuration.tab).append("</br>Zip file contents are loading...");
+    var btn = document.createElement("button");
+    btn.id = "zipLoadBtn";
+    btn.className = "btn btn-default";
+    btn.innerText = "Load Zip Contents";
+    $(Configuration.tab).append(btn);
 
-    var xhr = new XMLHttpRequest();
-    xhr.tab = Configuration.tab;
-    xhr.onreadystatechange = function(response) {
-        if (this.readyState == 4 && this.status == 200){
-            var data = this.response;
-            var box = this.tab;
+    var initialTab = Configuration.tab;
+    $(document).ready(function() {
+        $("#zipLoadBtn").click(function () {
+            loadZipContents(initialTab);
+            $("zipLoadBtn").hide();
+        });
+    });
 
-            zip.createReader(new zip.BlobReader(data), function(reader) {
-                // get all entries from the zip
-                reader.getEntries(function(entries) {
-                    if (entries.length) {
-                        var content = "</br><p>"+entries.length+" contents listed in zip file:</p></br>";;
-                        content += "<ul>"
-                        for (var e = 0; e < entries.length; e++) {
-                            content += "<li>"+entries[e]['filename']+"</li>"
+    var loadZipContents = function() {
+        console.log("Button clicked!");
+        $(initialTab).append("<link rel=\"stylesheet\"' href=\"@routes.Assets.at(\"stylesheets/glyphicon-animations.css\")\">")
+        $(initialTab).append("<span class='glyphicon glyphicon-refresh glyphicon-animation-spin glyphicon-2x'></span> Zip file contents are loading...");
+
+        var xhr = new XMLHttpRequest();
+        xhr.tab = initialTab;
+        xhr.onreadystatechange = function (response) {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = this.response;
+                var box = this.tab;
+
+                zip.createReader(new zip.BlobReader(data), function (reader) {
+                    // get all entries from the zip
+                    reader.getEntries(function (entries) {
+                        if (entries.length) {
+                            var content = "</br><p>" + entries.length + " contents listed in zip file:</p></br>";
+                            ;
+                            content += "<ul>"
+                            for (var e = 0; e < entries.length; e++) {
+                                content += "<li>" + entries[e]['filename'] + "</li>"
+                            }
+                            content += "</ul>";
+                            $(box).html(content);
                         }
-                        content += "</ul>";
-                        $(box).html(content);
-                    }
+                    });
                 });
-            });
-        }
-    };
-    xhr.open('GET', Configuration.url);
-    xhr.responseType = 'blob';
-    xhr.send();
+            }
+        };
+        xhr.open('GET', Configuration.url);
+        xhr.responseType = 'blob';
+        xhr.send();
+    }
 
 }(jQuery, Configuration));
