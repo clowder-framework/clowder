44d17c1d5bb6009b3d129cea57072300ba22a719
toddn <tcnichol@illinois.edu>
2019-12-11 13:52:02 -0600

    remove commented out methods

--- app/services/rabbitmq/RabbitmqExtractionBusService.scala
+++ app/services/rabbitmq/RabbitmqExtractionBusService.scala
@@ -28,54 +28,6 @@ import services.{CancellationMessage, DI, DatasetService, Entity, ExtractionBusS
 import scala.concurrent.{Await, Future}
 import scala.util.Try
 
-///**
-// * Despite the `fileId` be named as such, it is currently serialized as `id` and used as the id of any resource in
-// * question. So this will be the preview id in the case of messages operating on a preview, it will be the dataset
-// * id in the case of dataset messages (yes there is also `datasetId`, pyclowder2 checks that the two are the same.
-// * FIXME make optional fields Option[UUID]
-// * FIXME rename fileId to id and add a resourceType field (dataset, file, preview, etc.)
-// * TODO drop `intermediateId` or figure out how it works and use accordingly
-// * @param fileId this should only be used as
-// * @param intermediateId
-// * @param host
-// * @param queue
-// * @param metadata
-// * @param fileSize
-// * @param datasetId
-// * @param flags
-// * @param secretKey
-// */
-//case class ExtractorMessage(
-//                             msgid: UUID,
-//                             fileId: UUID,
-//                             notifies: List[String],
-//                             intermediateId: UUID,
-//                             host: String,
-//                             queue: String,
-//                             metadata: Map[String, Any],
-//                             fileSize: String,
-//                             datasetId: UUID = null,
-//                             flags: String = "",
-//                             secretKey: String,
-//                             // new fields
-//                             routing_key: String,
-//                             source: Entity,
-//                             activity: String,
-//                             target: Option[Entity]
-//                           )
-
-// TODO add other optional fields
-//case class Entity(
-//                   id: ResourceRef,
-//                   mimeType: Option[String],
-//                   extra: JsObject
-//                 )
-//
-//case class CancellationMessage(id: UUID, queueName: String, msgid: UUID)
-//
-//object Entity {
-//  implicit val implicitEntityWrites = Json.format[Entity]
-//}
 
 /**
   * Rabbitmq service.
