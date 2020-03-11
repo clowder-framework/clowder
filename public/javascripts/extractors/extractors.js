1fe70e41cfde5853cd012f98efdb28402b33dc99
toddn <todd.nicholson@gmail.com>
2020-03-11 14:40:42 -0500

    button present, modal pops up, does not actually delete and redirect yet

---
+++ public/javascripts/extractors/extractors.js
@@ -0,0 +1,32 @@
+function removeExtractor(fileId, isreload, url){
+
+    console.log('removing extractor');
+
+    var request = jsRoutes.api.Files.removeFile(fileId).ajax({
+        type: 'DELETE'
+    });
+
+    request.done(function (response, textStatus, jqXHR){
+        if(isreload === "true")
+            window.location.href=url;
+        else {
+            var obj = $('#'+ fileId+'-tile');
+            $('#masonry').masonry( 'remove', obj );
+            $('#masonry').masonry( 'layout' );
+            $('#'+ fileId+'-listitem').remove();
+
+            // After deleting the last file from a folder, display the empty message
+            if($("#tile-view").find("div[id$='-listitem']").length == 0){
+                $('#empty-folder-div').show();
+            }
+        }
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occured: " + textStatus, errorThrown);
+        var errMsg = "You must be logged in to delete a file from the system.";
+        if (!checkErrorAndRedirect(jqXHR, errMsg)) {
+            notify("The file was not deleted from the system due to : " + errorThrown, "error");
+        }
+    });
+}
