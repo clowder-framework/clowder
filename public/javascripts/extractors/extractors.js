3981e91e3f5014067556ba53e6c8e4ce153729cb
Mike Lambert <lambert8@illinois.edu>
2020-09-30 15:07:42 -0500

    Extractor Catalog: Extractor Maturity features (#69)
    * Add "maturity" field to ExtractorInfo, and an API method to update it
    
    * Fill out maturity column and dropdown buttons in the table view
    
    * Use generic PUT to update ExtractorInfo, don't add a new API call
    
    * Minor cleanup
    
    * Update app/models/Extraction.scala
    
    * Remove PUT /extractors/{name} from swagger

--- public/javascripts/extractors/extractors.js
+++ public/javascripts/extractors/extractors.js
@@ -1,3 +1,7 @@
+function redirectToSelectExtractors() {
+    window.location.href = jsRoutes.controllers.Extractors.selectExtractors().url;
+}
+
 function removeExtractor(extractorName){
 
     console.log('removing extractor', extractorName);
@@ -7,8 +11,7 @@ function removeExtractor(extractorName){
     });
 
     request.done(function (response, textStatus, jqXHR){
-        var url = jsRoutes.controllers.Extractors.selectExtractors().url;
-        window.location.href=url;
+        redirectToSelectExtractors();
     });
 
     request.fail(function (jqXHR, textStatus, errorThrown){
@@ -18,4 +21,64 @@ function removeExtractor(extractorName){
             notify("The extractor was not deleted from the system due to : " + errorThrown, "error");
         }
     });
+}
+
+function fetchExtractorInfo(extractorName) {
+    console.log('fetching extractor info: ', extractorName);
+
+    var request = jsRoutes.api.Extractions.getExtractorInfo(extractorName).ajax({
+        type: 'GET'
+    });
+
+    request.done(function (response, textStatus, jqXHR){
+        console.log("fetched extractor info:", response);
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occurred: " + textStatus, errorThrown);
+        var errMsg = "Something went wrong deleting extractor.";
+        if (!checkErrorAndRedirect(jqXHR, errMsg)) {
+            notify("The extractor was not deleted from the system due to : " + errorThrown, "error");
+        }
+    });
+
+    return request;
+}
+
+function updateExtractorInfo(extractorInfo) {
+    var extractorName = extractorInfo['name']
+    console.log('updating extractor info: ', extractorName);
+    var request = jsRoutes.api.Extractions.addExtractorInfo().ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(extractorInfo)
+    });
+
+    request.done(function (response, textStatus, jqXHR){
+        redirectToSelectExtractors();
+        console.log("Updated extractor info: " + extractorName);
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("The following error occurred: " + textStatus, errorThrown);
+        var errMsg = "Something went wrong updating extractor.";
+        if (!checkErrorAndRedirect(jqXHR, errMsg)) {
+            notify("The extractor was not updated due to : " + errorThrown, "error");
+        }
+    });
+
+    // Return false to cancel browser's default handling of href
+    return false;
+}
+
+function changeMaturity(extractorName, newMaturity) {
+    console.log('updating extractor maturity: ', extractorName + ' -> ' + newMaturity);
+    var fetchReq = fetchExtractorInfo(extractorName);
+
+    fetchReq.done(function (extractorInfo, textStatus, jqXHR) {
+        extractorInfo['maturity'] = newMaturity;
+        updateExtractorInfo(extractorInfo);
+    });
+
+    return false;
 }
