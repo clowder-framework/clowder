aa3fe5b7cad9868d42447ea9bc01fd676d459736
Michael D. Johnson <mdjohnson44@gmail.com>
2020-11-20 13:29:28 -0600

    updating comments. adding update if job_id exists

--- scripts/mongodb/UpdateUserId.js
+++ scripts/mongodb/UpdateUserId.js
@@ -1,6 +1,6 @@
 /***
-This code iterates through each document where user_id does not exist, checks if file_id exists
-in uploads.files collection. If it exists, it grabs the author._id for use as user_id. If it
+This code iterates through each document where user_id does not exist and checks if file_id exists
+in uploads.files collection. If a file exists, it grabs the author._id for use as user_id. If it
 does not exist (or if author._id is null), it searches for the file_id in the datasets collection.
 If found, it gets the author._id for use as the user_id. If an author id is found and not null
 an update to the extractions collection is made by adding user_id: author._id. 
@@ -8,14 +8,15 @@ an update to the extractions collection is made by adding user_id: author._id.
 
 db.extractions.find({"user_id":{$exists: 0}}).forEach(function(ext) {
     let authorID = null;
-    // Looping through each extraction where user_id doesn't exist...
-    // Look up file_id in uploads.files, get author.id if found
+    // Looping through each extraction where user_id doesn't exist,
+    // if file_id found in uploads.files, get author._id
     let foundFile = db.uploads.files.findOne({"_id": ext.file_id})
     if (foundFile != null) {
         authorID = foundFile.author._id;
     }
 
-    // if not found in uploads.files, look up file_id in datasets, get author.id if found
+    // If file not found in uploads.files or if author._id doesn't exist,
+    // look up file_id in datasets, get author.id if found
     if (foundFile == null || authorID == null) {
         let foundAuthorInDatasets = db.datasets.findOne({"files": {$in: [ext.file_id]}});
         if (foundAuthorInDatasets != null) {
@@ -23,11 +24,23 @@ db.extractions.find({"user_id":{$exists: 0}}).forEach(function(ext) {
         }
     }
     if (authorID != null) {
-        // update user_id for entry in extractions database
-        db.extractions.update({"_id": ext._id}, {
-            "$set": {
-                "user_id": authorID
-            }
-        });
+        // If job_id exists update author._id for all documents with job_id,
+        // else update based on the current document id.
+        if (ext.job_id != null) {
+            // Update user_id for entry in extractions database
+            db.extractions.update({"job_id": ext.job_id}, {
+                "$set": {
+                    "user_id": authorID
+                }
+            });
+        }
+        else {
+            // Update user_id for entry in extractions database
+            db.extractions.update({"_id": ext._id}, {
+                "$set": {
+                    "user_id": authorID
+                }
+            });
+        }
     }
 });
