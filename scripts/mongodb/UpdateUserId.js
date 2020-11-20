44f4bbd9fd32dfa40d44c0e5c931d5fef26b51a2
Michael D. Johnson <michaeldjohnson@control.local>
2020-11-20 10:06:40 -0600

    adding script to add user_id to documents in extractions collection if does not exist

---
+++ scripts/mongodb/UpdateUserId.js
@@ -0,0 +1,34 @@
+/***
+This code iterates through each document where user_id does not exist, checks if file_id exists
+in uploads.files collection. If it exists, it grabs the author._id for use as user_id. If it
+does not exist (or if author._id is null), it searches for the file_id in the datasets collection.
+If found, it gets the author._id for use as the user_id. If an author id is found and not null
+an update to the extractions collection is made by adding user_id: author._id. 
+***/
+
+let authorID = null;
+
+db.extractions.find({"user_id":{$exists: 0}}).forEach(function(ext) {
+    // Looping through each extraction where user_id doesn't exist...
+    // Look up file_id in uploads.files, get author.id if found
+    let foundFile = db.uploads.files.findOne({"_id": ext.file_id})
+    if (foundFile != null) {
+        authorID = foundFile.author._id;
+    }
+
+    // if not found in uploads.files, look up file_id in datasets, get author.id if found
+    if (foundFile == null || authorID == null) {
+        let findAuthorInDatasets = db.datasets.findOne({"files": {$in: [ext.file_id]}});
+        if (findAuthorInDatasets != null) {
+            authorID = findAuthorInDatasets.author._id;
+        }
+    }
+    if (authorID != null) {
+        // update user_id for entry in extractions database
+        db.extractions.update({"_id": ext._id}, {
+            "$set": {
+                "user_id": authorID
+            }
+        });
+    }
+});
