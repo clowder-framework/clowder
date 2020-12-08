3ebfe38610d32c70891b9f3d771e6d6ecbab1fd2
toddn <tcnichol@illinois.edu>
2020-12-07 20:39:07 -0600

    files are now removed properly count is still wrong nothing under selections tab works for files

--- app/assets/javascripts/select-bulk.js
+++ app/assets/javascripts/select-bulk.js
@@ -26,4 +26,33 @@ $(function() {
 
         }
     });
+
+    $('.select-file').click(function  () {
+        var file_id = $(this).data("id");
+        console.log('clicked a file', file_id)
+
+        if ($("a[data-id='"+file_id+"'] span.glyphicon").hasClass('glyphicon-ok')) {
+
+            var request = jsRoutes.api.Selected.remove().ajax({
+                data: JSON.stringify({"file":file_id}),
+                type: 'POST',
+                contentType: "application/json"
+            });
+
+            request.done(function (response, textStatus, jqXHR){
+                console.log("file " + file_id + " removed");
+                var elm = document.getElementById(file_id+"-tile");
+                console.log(elm);
+                elm.parentElement.removeChild(elm);
+                updateSelectedCount(response.selected_count)
+                updateMessage();
+            });
+
+            request.fail(function (jqXHR, textStatus, errorThrown){
+                console.error("The following error occured: "+ textStatus, errorThrown);
+                window.location = "../login"; // FIXME hardcoded
+            });
+
+        }
+    });
 });
