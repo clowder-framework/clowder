8d9b1c3320dde50f5df5796b1ed512d8b3d171d4
Max Burnette <mburnet2@illinois.edu>
2020-12-15 10:04:48 -0600

    re-add env

---
+++ app/util/silhouette/Env.scala
@@ -0,0 +1,13 @@
+package util.silhouette.auth
+
+import com.mohiva.play.silhouette.api.Env
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import models.User
+
+/**
+ * The default env.
+ */
+trait ClowderEnv extends Env {
+  type I = User
+  type A = CookieAuthenticator
+}
