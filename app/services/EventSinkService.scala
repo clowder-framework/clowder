32562637623b66b1ccaf86a14e780d9982f763bf
Mike Lambert <lambert8@illinois.edu>
2021-01-26 12:11:12 -0600

    Filled out EventSinkService impl

--- app/services/EventSinkService.scala
+++ app/services/EventSinkService.scala
@@ -1,6 +1,6 @@
 package services
 
-import models.{Collection, Dataset, ExtractorInfo, File, User}
+import models.{Collection, Dataset, ExtractorInfo, File, ProjectSpace, User}
 
 import java.net.URI
 import java.time.Instant
@@ -8,15 +8,9 @@ import play.api.{Logger, Play}
 import play.api.Play.current
 import play.api.libs.json.{JsValue, Json}
 
-// TODO: Wire up to real MessageService
-class MockMessageService {
-  def submit(exchange: String, routing_key: String, metadata: JsValue) = {
-    Logger.info("Message submitted to event sink exchange: " + Json.stringify(metadata))
-  }
-}
-
 class EventSinkService {
-  val messageService: MockMessageService = new MockMessageService() // DI.injector.getInstance(classOf[MessageService])
+  val messageService: MessageService = DI.injector.getInstance(classOf[MessageService])
+  val userService: UserService = DI.injector.getInstance(classOf[UserService])
 
   /** Event Sink exchange name in RabbitMQ */
   lazy val exchangeName = {
@@ -32,10 +26,10 @@ class EventSinkService {
     Logger.info("eventsink.exchangename=" + exchangeName)
     Logger.info("eventsink.queueName=" + queueName)
 
+    Logger.info("Submitting message to event sink exchange: " + Json.stringify(metadata))
+
     //val message = EventSinkMessage(Instant.now().getEpochSecond, category, metadata)
     messageService.submit(exchangeName, queueName, metadata)
-
-    Logger.info("Message submitted to event sink exchange: " + Json.stringify(metadata))
   }
 
   /** Log an event when user views a dataset */
@@ -68,7 +62,7 @@ class EventSinkService {
 
   /** Log an event when user views a collection */
   def logCollectionViewEvent(collection: Collection, viewer: Option[User]) = {
-    Logger.info("User viewed a file: " + collection.id.stringify)
+    Logger.info("User viewed a collection: " + collection.id.stringify)
     logEvent("view_resource", Json.obj(
       "type" -> "collection",
       "resource_id" -> collection.id,
@@ -80,25 +74,137 @@ class EventSinkService {
     ))
   }
 
-  /** TBD - Not Currently Implemented */
-  def logSubmitFileToExtractorEvent(file: File, extractor: ExtractorInfo, submitter: Option[User]) = {
+  /** Log an event when user views a space */
+  def logSpaceViewEvent(space: ProjectSpace, viewer: Option[User]) = {
+    Logger.info("User viewed a space: " + space.id.stringify)
+    (viewer, userService.get(space.creator)) match {
+      case (Some(v), Some(author)) => {
+        logEvent("view_resource", Json.obj(
+          "type" -> "space",
+          "resource_id" -> space.id,
+          "resource_name" -> space.name,
+          "author_id" -> space.creator.stringify,
+          "author_name" -> author.fullName,
+          "viewer_id" -> v.id,
+          "viewer_name" -> v.getMiniUser.fullName
+        ))
+      }
+      case (None, Some(author)) => {
+        logEvent("view_resource", Json.obj(
+          "type" -> "space",
+          "resource_id" -> space.id,
+          "resource_name" -> space.name,
+          "author_id" -> author.id,
+          "author_name" -> author.fullName,
+          "viewer_id" -> "",
+          "viewer_name" -> "Anonymous"
+        ))
+      }
+      case (Some(v), None) => {
+        // TODO: Is this a real case? Is this needed?
+        logEvent("view_resource", Json.obj(
+          "type" -> "space",
+          "resource_id" -> space.id,
+          "resource_name" -> space.name,
+          "author_id" -> space.creator.stringify,
+          "author_name" -> "",
+          "viewer_id" -> v.id,
+          "viewer_name" -> v.getMiniUser.fullName
+        ))
+      }
+      case (None, None) => {
+        // TODO: Is this a real case? Is this needed?
+        logEvent("view_resource", Json.obj(
+          "type" -> "space",
+          "resource_id" -> space.id,
+          "resource_name" -> space.name,
+          "author_id" -> space.creator.stringify,
+          "author_name" -> "",
+          "viewer_id" -> "",
+          "viewer_name" -> "Anonymous"
+        ))
+      }
+    }
+  }
 
+  def logSubmitFileToExtractorEvent(file: File, extractor: ExtractorInfo, submitter: Option[User]) = {
+    logEvent("extraction", Json.obj(
+      "type" -> "file",
+      "extractor_name" -> extractor.name,
+      "resource_id" -> file.id,
+      "resource_name" -> file.filename,
+      "author_id" -> file.author.id,
+      "author_name" -> file.author.fullName,
+      "submitter_id" -> submitter.get.id,
+      "submitter_name" -> submitter.get.getMiniUser.fullName
+    ))
   }
 
   def logSubmitDatasetToExtractorEvent(dataset: Dataset, extractor: ExtractorInfo, submitter: Option[User]) = {
-
+    logEvent("extraction", Json.obj(
+      "type" -> "dataset",
+      "extractor_name" -> extractor.name,
+      "resource_id" -> dataset.id,
+      "resource_name" -> dataset.name,
+      "author_id" -> dataset.author.id,
+      "author_name" -> dataset.author.fullName,
+      "submitter_id" -> submitter.get.id,
+      "submitter_name" -> submitter.get.getMiniUser.fullName
+    ))
   }
 
-  def logFileUploadEvent(file: File, uploader: Option[User]) = {
-
+  def logSubmitSelectionToExtractorEvent(dataset: Dataset, extractor: ExtractorInfo, submitter: Option[User]) = {
+    logEvent("extraction", Json.obj(
+      "type" -> "selection",
+      "extractor_name" -> extractor.name,
+      "resource_id" -> dataset.id,
+      "resource_name" -> dataset.name,
+      "author_id" -> dataset.author.id,
+      "author_name" -> dataset.author.fullName,
+      "submitter_id" -> submitter.get.id,
+      "submitter_name" -> submitter.get.getMiniUser.fullName
+    ))
   }
 
-  def logFileDownloadEvent(file: File, downloader: Option[User]) = {
+  def logFileUploadEvent(file: File, dataset:Dataset, uploader: Option[User]) = {
+    logEvent("upload", Json.obj(
+      "dataset_id" -> dataset.id,
+      "dataset_name" -> dataset.name,
+      "dataset_author_name" -> dataset.author.fullName,
+      "dataset_author_id" -> dataset.author.id,
+      "uploader_id" -> uploader.get.id,
+      "uploader_name" -> uploader.get.getMiniUser.fullName,
+      "filename" -> file.filename,
+      "length" -> file.length
+    ))
+  }
 
+  def logFileDownloadEvent(file: File, /*dataset: Dataset,*/ downloader: Option[User]) = {
+    logEvent("download", Json.obj(
+      /*"dataset_id" -> dataset.id,
+      "dataset_name" -> dataset.name,
+      "dataset_author_name" -> dataset.author.fullName,
+      "dataset_author_id" -> dataset.author.id,*/
+      "type" -> "file",
+      "downloader_id" -> downloader.get.id,
+      "downloader_name" -> downloader.get.getMiniUser.fullName,
+      "filename" -> file.filename,
+      "length" -> file.length
+    ))
   }
 
   def logDatasetDownloadEvent(dataset: Dataset, downloader: Option[User]) = {
-
+    logEvent("download", Json.obj(
+      "type" -> "dataset",
+      "dataset_id" -> dataset.id,
+      "dataset_name" -> dataset.name,
+      "dataset_author_name" -> dataset.author.fullName,
+      "dataset_author_id" -> dataset.author.id,
+      "downloader_id" -> downloader.get.id,
+      "downloader_name" -> downloader.get.getMiniUser.fullName,
+      "files_length" -> dataset.files.length,
+      "folder_length" -> dataset.folders.length
+    ))
   }
 }
 
