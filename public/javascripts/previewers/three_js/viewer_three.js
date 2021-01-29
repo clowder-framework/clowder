072d70374543d7a9df190b6491eb0eee6fc4a15f
Valentino Constantinou <cconsta1@alumni.nd.edu>
2021-01-29 21:19:59 +0200

    Adding comments to viewer_three.js

--- public/javascripts/previewers/three_js/viewer_three.js
+++ public/javascripts/previewers/three_js/viewer_three.js
@@ -18,38 +18,37 @@
   Wish list:
 
   1) Become able to upload zip files into clowder which will be 
-  automatically unzipped. Subsequntly, the files they contain will be 
-  accessible to the previewer. This is necessary for PLY files which come 
-  with a texture file. (NEXUS files are standalone files and their
-  texture is incorporated into the file)
+  automatically unzipped. Subsequently, the files they contain will be
+  accessible to the previewer. This is necessary for 3D models which come
+  with a texture file.
 
-  2) Figure out how to load a GLTF file which comes with texture files.
-  Notice that if you just load the GLTF file, things work properly anyhow, 
-  but I think this happens because of the BIN file contaning information
+  2) Figure out how to load GLTF files with texture files manually.
+  Note that the GLTFLoader() loads everything automatically, if they
+  are downloaded from sketchfab. I think the BIN file contains information
   about how the textures will be loaded.
 
   3) Figure out how the user will interact with the model smoothly using
-  OrbitControls.
+  OrbitControls().
 
   4) Figure out how to extract the dimensions of the model and then adjust
-  the dimensions so that the model displays properly in the browser. (I noticed
-  that some models display really big or really small).
+  the dimensions so that the model displays properly in the browser. (Some models
+  display really big or really small).
 
   5) For the moment, we are loading the three.js files from the folder js. Specifically,
   I found the files "three.min.js", "GLTFLloader.js", "OrbitControls.js" from the GitHub
-  of a guy who created a website to dispay a single GLTF model (yes, I like stealing ideas :P).
+  of a guy who created a website to display a single GLTF model.
   However, the three.js library is available online and we should gradually switch to loading
-  the library from an online server (preferably an https server, in a fashion similar to what
-  we are doing in "viewer_hop.js").
+  the library from an online server (preferably an https server, in a fashion similar to what's
+  done in "viewer_hop.js").
 
   6) So far we are able to load GLTF models using the GLTF Loader. I believe we can easily
-  load 3D models which come in a different format using for example the FBX loader. We need
+  load 3D models which come in a different format using for example the FBXLoader. We need
   to extract the extension of the file and depending of the type of the file, we can use the 
   appropriate loader. This is exactly what we are doing in viewer_hop.js lines 61-70. 
 
   Note: You can come up with your own ideas about how to attack these issues, add your own
   items in the wish list, and whenever you make changes to the code, you can add them on the 
-  top like I do. Example: adding something + 01/24/21 (cc) Added smthg. removing smthg 
+  top like I do. Example: adding something + 01/24/21 (cc) Added smthg. Removing smthg
   - 01/24/21 (cc) Removed smthg. (Your initials are mt). Finally, once we have something 
   functional, we can create a pull request with clowder and contribute our code. Also, we need
   to add you into the list of contributors. :)
