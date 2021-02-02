bedb500e3525d7645d8f48a6edaebf7474fa9fb7
Valentino Constantinou <cconsta1@alumni.nd.edu>
2021-02-02 11:09:43 +0200

    Functional FBV previewer

--- public/javascripts/previewers/three_js/viewer_three.js
+++ public/javascripts/previewers/three_js/viewer_three.js
@@ -55,11 +55,6 @@
 
 ****************************************************************/
 
-// The function below is a jQuery function. The exact details
-// about how it works are not completely clear to me but the fact is that it works.
-// Configuration is an object that looks like Configuration = {tab = "tab",
-// previewer = "..."}. We access Configuration using the dot and then the name of the variable
-
 (function ($, Configuration) {
 
     let useTab = Configuration.tab;
@@ -85,13 +80,12 @@
     // These libraries can be found online and eventually we should load
     // them from there
 
-    let scripts =["three.min.new.js", "inflate.min.js",
-        "GLTFLloader.new.js", "OrbitControls.new.js", "FBXLoader.new.js"];
+    let scripts =[ "three.min.js",  "stats.min.js", "OrbitControls.js", "FBXLoader.js", "fflate.min.js" ];
 
     // We use the for loop to load the files into our program
 
     for (index = 0; index < scripts.length; index++) {
-        var s = document.createElement("script");
+        let s = document.createElement("script");
         s.type = "text/javascript";
         s.src = previewer + "/js/" + scripts[index];
         $(useTab).append(s);
@@ -100,98 +94,102 @@
     // $(document.ready()) is a jQuery function inside which you can call other functions
     // to run your program
 
-    $(document).ready(function(){
-        init();
-    });
+     $(document).ready(function(){
+         init();
+         animate();
+     });
 
 
 }(jQuery, Configuration));
 
-// Here is the gist of the work. init() is the function we use to load the 3D
-// model. We declare all those variables outside init(), so that they are now global
-// variables (i.e., they can be used anywhere in this file. Variables declared inside
-// a function are instead called local variables and they can be used inside the function.
+let camera, scene, renderer, stats;
 
-// Every three.js program needs a camera that sits in frond of the model and shows it on
-// the screen, much like what we do in the real world. The scene is the scene where we put the
-// model the background colors, the camera, etc. And the renderer puts all of these things together.
+const clock = new THREE.Clock();
 
-// I wish I could explain what a mesh is. The loader is what loads the model.
+let mixer;
 
-let camera, scene, renderer;
-let mesh;
-let loader;
-
-// These are variables I use to define the dimensions of the window. The container
-// is the small window that shows up on the screen. You can change the numbers and see what will happen.
-
-let container;
 let previewerWidth = 640;
 let previewerHeight = 480;
 
-
 function init() {
-    // Here is where I tell the program to create a window inside clowder and
-    // show everything within that window.
 
-    container = document.getElementById(Configuration.tab.replace("#",""));
+    const container = document.getElementById(Configuration.tab.replace("#",""));
 
-    // Here we create a scene
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.set( 100, 200, 300 );
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xdddddd);
+    scene.background = new THREE.Color( 0xa0a0a0 );
+    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
 
-    // Here is where I create a camera. And I really need your help to understand how things work.
-    // This site is good but I didn't study it thoroughly:
-    // https://threejsfundamentals.org/threejs/lessons/threejs-fundamentals.html
-    // You can check this as well:
-    // https://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set( 0, 200, 0 );
+    scene.add( hemiLight );
 
-    camera = new THREE.PerspectiveCamera( 70, previewerWidth/previewerHeight, 1, 1000 );
-    camera.position.z = 40;
+    const dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( 0, 200, 100 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = - 100;
+    dirLight.shadow.camera.left = - 120;
+    dirLight.shadow.camera.right = 120;
+    scene.add( dirLight );
 
-    // Here is where we load the texture.
-    
-    // Here is where we import the 3D model.
+    // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
-    // For now we save it in the clowder installation,
-    // inside the folder public/javascripts/previewers/three_js/models
+    // ground
+    const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+    mesh.rotation.x = - Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add( mesh );
 
-    //let loader = new THREE.GLTFLoader();
-    //let filepath = "/assets/javascripts/previewers/three_js/models/airplane/scene.gltf";
-    let loader = new THREE.FBXLoader();
-    let filepath = "/assets/javascripts/previewers/three_js/models/free-1972-datsun-240k-gt/source/datsun240k.fbx";
+    const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add( grid );
 
-    // This is the loader function. I don't understand all the details
+    // model
+    const loader = new THREE.FBXLoader();
+    loader.load( '/assets/javascripts/previewers/three_js/models/samba/Samba_Dancing.fbx', function ( object ) {
 
-    loader.load(filepath, function (obj) {
+        mixer = new THREE.AnimationMixer( object );
 
-        // For the GLTF loader you need to add obj.scene to the scene
+        const action = mixer.clipAction( object.animations[ 0 ] );
+        action.play();
 
-        //scene.add(obj.scene);
+        object.traverse( function ( child ) {
 
-        // For the FBX loader you need to add the obj itself.
+            if ( child.isMesh ) {
 
-        scene.add(obj);
+                child.castShadow = true;
+                child.receiveShadow = true;
 
-        let light = new THREE.AmbientLight( 0xffffff );
-        scene.add( light );
+            }
 
-        animate();
-    });
+        } );
 
-    // Here we initialize the renderer which puts everything together as my understanding is for now.
+        scene.add( object );
+
+    } );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( previewerWidth, previewerHeight );
-    container.appendChild( renderer.domElement );
+    renderer.shadowMap.enabled = true;
 
-    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+    const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 100, 0 );
+    controls.update();
     container.appendChild(renderer.domElement);
-}
 
-// The functions below where found elsewhere and honestly I don't understand how they work.
+    window.addEventListener( 'resize', onWindowResize );
+
+    // stats
+    stats = new Stats();
+    //container.appendChild( stats.dom );
+
+
+}
 
 function onWindowResize() {
 
@@ -199,11 +197,22 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
+
+//
 
 function animate() {
 
     requestAnimationFrame( animate );
+
+    const delta = clock.getDelta();
+
+    if ( mixer ) mixer.update( delta );
+
     renderer.render( scene, camera );
+
+    stats.update();
+
 }
 
