f0139a48bec93446f4bb0e62680f734543561c9b
Valentino Constantinou <cconsta1@alumni.nd.edu>
2021-02-08 23:40:59 +0200

    Added comments

--- public/javascripts/previewers/three_js/viewer_three.js
+++ public/javascripts/previewers/three_js/viewer_three.js
@@ -24,10 +24,10 @@
   accessible to the previewer. This is necessary for 3D models which come
   with a texture file.
 
-  2) Figure out how to load GLTF files with texture files manually.
-  Note that the GLTFLoader() loads everything automatically, if they
-  are downloaded from sketchfab. I think the BIN file contains information
-  about how the textures will be loaded.
+  2) Figure out how to load FBX files with texture files manually.
+  Note that the FBXLoader() loads everything automatically, if they
+  are downloaded from sketchfab. For the GLTF loader, I think the BIN file contains information
+  about how the textures are loaded.
 
   3) Figure out how the user will interact with the model smoothly using
   OrbitControls().
@@ -37,16 +37,18 @@
   display really big or really small).
 
   5) For the moment, we are loading the three.js files from the folder js. Specifically,
-  I found the files "three.min.js", "GLTFLloader.js", "OrbitControls.js" from the GitHub
-  of a guy who created a website to display a single GLTF model.
+  I found the files "three.min.js", "FBXloader.js", "OrbitControls.js" from
+  https://cdn.jsdelivr.net/npm/three@0.117.1/.
   However, the three.js library is available online and we should gradually switch to loading
   the library from an online server (preferably an https server, in a fashion similar to what's
   done in "viewer_hop.js").
 
-  6) So far we are able to load GLTF models using the GLTF Loader. I believe we can easily
-  load 3D models which come in a different format using for example the FBXLoader. We need
+  6) So far we are able to load FBX models using the FBX Loader. I believe we can easily
+  load 3D models which come in a different format using for example the GLTFLoader. We need
   to extract the extension of the file and depending of the type of the file, we can use the 
-  appropriate loader. This is exactly what we are doing in viewer_hop.js lines 61-70. 
+  appropriate loader. This is exactly what we are doing in viewer_hop.js lines 61-70.
+
+  7) Add the option to go full screen, and add additional tools.
 
   Note: You can come up with your own ideas about how to attack these issues, add your own
   items in the wish list, and whenever you make changes to the code, you can add them on the 
@@ -102,8 +104,6 @@
          init(referenceUrl);
          animate();
      });
-
-
 
 }(jQuery, Configuration));
 
