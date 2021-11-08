8a6906d484d399f91a2de28ca3680d2a6ec4837d
Valentino Constantinou <cconsta1@alumni.nd.edu>
2021-11-08 13:04:04 +0200

    Added loading screen

---
+++ public/javascripts/previewers/three_js/README.md
@@ -0,0 +1,5 @@
+# three_js
+
+three_js utilizes the popular three.js 3D library,
+to allow a user view his/her 3D FBX and GLTF
+files online.
--- public/javascripts/previewers/three_js/viewer_three.js
+++ public/javascripts/previewers/three_js/viewer_three.js
@@ -13,131 +13,106 @@
  * + 11/21/20 (cc): Code development started
  * + 02/08/21 (cc): Ability to load from url added.
  *                  First functional version
+ * + 11/08/21 (cc): Added loading screen and ability
+ *                  to load gltf files
 
-****************************************************************/
+ ****************************************************************/
 
 /****************************************************************
-  Wish list:
+ Wish list:
 
-  1) Become able to upload zip files into clowder which will be
-  automatically unzipped. Subsequently, the files they contain will be
-  accessible to the previewer. This is necessary for 3D models which come
-  with a texture file.
+ 1) Become able to upload zip files into clowder which will be
+ automatically unzipped. Subsequently, the files they contain will be
+ accessible to the previewer. This is necessary for 3D models which come
+ with a texture file.
 
-  2) Figure out how to load FBX files with texture files manually.
-  Note that the FBXLoader() loads everything automatically, if they
-  are downloaded from sketchfab. For the GLTF loader, I think the BIN file contains information
-  about how the textures are loaded.
+ 2) Figure out how to load FBX files with texture files manually.
+ Note that the FBXLoader() loads everything automatically, if they
+ are downloaded from sketchfab. For the GLTF loader, I think the BIN file contains information
+ about how the textures are loaded.
 
-  3) Figure out how the user will interact with the model smoothly using
-  OrbitControls().
+ 3) For the moment, we are loading the three.js files from the folder js. Specifically,
+ I found the files "three.min.js", "FBXloader.js", "OrbitControls.js" from
+ https://cdn.jsdelivr.net/npm/three@0.117.1/.
+ However, the three.js library is available online and we should gradually switch to loading
+ the library from an online server (preferably an https server, in a fashion similar to what's
+ done in "viewer_hop.js").
 
-  4) Figure out how to extract the dimensions of the model and then adjust
-  the dimensions so that the model displays properly in the browser. (Some models
-  display really big or really small).
+ 4) Add the option to go full screen, and add additional tools.
 
-  5) For the moment, we are loading the three.js files from the folder js. Specifically,
-  I found the files "three.min.js", "FBXloader.js", "OrbitControls.js" from
-  https://cdn.jsdelivr.net/npm/three@0.117.1/.
-  However, the three.js library is available online and we should gradually switch to loading
-  the library from an online server (preferably an https server, in a fashion similar to what's
-  done in "viewer_hop.js").
-
-  6) So far we are able to load FBX models using the FBX Loader. I believe we can easily
-  load 3D models which come in a different format using for example the GLTFLoader. We need
-  to extract the extension of the file and depending of the type of the file, we can use the
-  appropriate loader. This is exactly what we are doing in viewer_hop.js lines 61-70.
-
-  7) Add the option to go full screen, and add additional tools.
-
-  Note: You can come up with your own ideas about how to attack these issues, add your own
-  items in the wish list, and whenever you make changes to the code, you can add them on the
-  top like I do. Example: adding something + 01/24/21 (cc) Added smthg. Removing smthg
-  - 01/24/21 (cc) Removed smthg. (Your initials are mt). Finally, once we have something
-  functional, we can create a pull request with clowder and contribute our code. Also, we need
-  to add you into the list of contributors. :)
-
-****************************************************************/
+ ****************************************************************/
 
 let fileNameExtension;
 let loader;
 
 (function ($, Configuration) {
-
     let useTab = Configuration.tab;
     let referenceUrl = Configuration.url;
     let previewer = Configuration.previewer;
 
-    // The following variables are not needed for now
+    $(useTab).append('<section id="loading-screen">\n' +
+        '\n' +
+        '\t<div id="loader"></div>\n' +
+        '\n' +
+        '</section>')
+
+    $(useTab).append('<link rel="stylesheet" type="text/css" href = "/assets/javascripts/previewers/three_js/loading_screen.css" >');
 
     let fileName = $('#file-name-title').text().trim();
     fileNameExtension = fileName.split('.').pop();
 
-    //let fileType;
-
-    console.log(fileName);
-    console.log(fileNameExtension);
-
-    let scripts =[ "three.min.js",  "stats.min.js", "OrbitControls.js", "FBXLoader.js", "GLTFLoader.js", "fflate.min.js" ];
-
+    let scripts = ["three.min.js", "stats.min.js", "OrbitControls.js", "FBXLoader.js", "GLTFLoader.js", "fflate.min.js"];
 
     for (let index = 0; index < scripts.length; index++) {
         let s = document.createElement("script");
         s.type = "text/javascript";
         s.src = previewer + "/js/" + scripts[index];
         $(useTab).append(s);
-       }
+    }
 
-    // $(document.ready()) is a jQuery function inside which you can call other functions
-    // to run your program
-
-     $(document).ready(function(){
-         init(referenceUrl);
-         animate();
-     });
+    $(document).ready(function () {
+        init(referenceUrl);
+        animate();
+    });
 
 }(jQuery, Configuration));
 
 let camera, scene, renderer, stats;
-
 const clock = new THREE.Clock();
-
 let mixer;
-
 let previewerWidth = 640;
 let previewerHeight = 480;
 
 function init(urlAddress) {
-    const container = document.getElementById(Configuration.tab.replace("#",""));
+    const container = document.getElementById(Configuration.tab.replace("#", ""));
 
-    renderer = new THREE.WebGLRenderer( { antialias: false, powerPreference: "high-performance" } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( previewerWidth, previewerHeight );
+    renderer = new THREE.WebGLRenderer({antialias: false, powerPreference: "high-performance"});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(previewerWidth, previewerHeight);
     renderer.shadowMap.enabled = true;
 
-
-    camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 2000000 );
-    camera.position.set( 0, 100, 1250 );
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 2000000);
+    camera.position.set(0, 100, 1250);
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xa0a0a0 );
+    scene.background = new THREE.Color(0x444444);
     //scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
 
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    hemiLight.position.set( 0, 200, 0 );
-    scene.add( hemiLight );
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 200, 0);
+    scene.add(hemiLight);
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 0, 200, 100 );
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(0, 200, 100);
     dirLight.castShadow = true;
     dirLight.shadow.camera.top = 180;
-    dirLight.shadow.camera.bottom = - 100;
-    dirLight.shadow.camera.left = - 120;
+    dirLight.shadow.camera.bottom = -100;
+    dirLight.shadow.camera.left = -120;
     dirLight.shadow.camera.right = 120;
-    scene.add( dirLight );
+    scene.add(dirLight);
 
-    const controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.target.set( 0, 100, 0 );
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 100, 0);
     controls.update();
     container.appendChild(renderer.domElement);
 
@@ -158,15 +133,19 @@ function init(urlAddress) {
     // grid.material.transparent = true;
     // scene.add( grid );
 
-    if(fileNameExtension ==='fbx') {
-        loader = new THREE.FBXLoader();
+    const loadingManager = new THREE.LoadingManager(() => {
+        const loadingScreen = document.getElementById('loading-screen');
+        loadingScreen.classList.add('fade-out');
+        loadingScreen.addEventListener('transitionend', onTransitionEnd);
+    });
+
+    if (fileNameExtension === 'fbx') {
+        loader = new THREE.FBXLoader(loadingManager);
     }
 
-    if(fileNameExtension ==='gltf') {
-        loader = new THREE.GLTFLoader();
+    if (fileNameExtension === 'gltf') {
+        loader = new THREE.GLTFLoader(loadingManager);
     }
-
-
 
     loader.setPath(urlAddress);
 
@@ -174,26 +153,11 @@ function init(urlAddress) {
 
     //const texture = new THREE.TextureLoader().setPath('/assets/javascripts/previewers/three_js/models/chimpanzee/source/');
 
-    loader.load( '', function ( object ) {
+    loader.load('', function (object) {
 
-        if(fileNameExtension ==='gltf') {
-            object=object.scene;
+        if (fileNameExtension === 'gltf') {
+            object = object.scene;
         }
-
-        // let box = new THREE.Box3();
-        // box.setFromObject(object);
-        //
-        // let size = new THREE.Vector3();
-        // box.getSize(size);
-        //
-        // let center = new THREE.Vector3();
-        // box.getCenter(center);
-        //
-        // let scaleTemp = new THREE.Vector3().copy(scaleV3).divide(size);
-        // let scale = Math.min(scaleTemp.x, Math.min(scaleTemp.y, scaleTemp.z));
-        //
-        // object.scale.setScalar(scale);
-        // object.position.sub(center.multiplyScalar( scale ));
 
         object.traverse(function (child) {
 
@@ -206,18 +170,12 @@ function init(urlAddress) {
 
         });
 
-        if( object.animations[ 0 ] ) {
-            mixer = new THREE.AnimationMixer( object );
+        if (object.animations[0]) {
+            mixer = new THREE.AnimationMixer(object);
 
-            const action = mixer.clipAction( object.animations[ 0 ] );
+            const action = mixer.clipAction(object.animations[0]);
             action.play();
         }
-
-        //object.position.setZ(-300);
-
-        //object.position.y = - previewerHeight / 2;
-        //object.position.x =  previewerWidth / 2;
-        //object.position.z = -1000;
 
         const box = new THREE.Box3().setFromObject(object);
         const size = box.getSize(new THREE.Vector3()).length();
@@ -229,34 +187,14 @@ function init(urlAddress) {
         object.position.y += (object.position.y - center.y);
         object.position.z += (object.position.z - center.z);
 
-
-        // console.log(object.position.x);
-        // console.log(object.position.y);
-        // console.log(object.position.z);
-
         controls.maxDistance = size * 10;
         camera.near = size / 100;
         camera.far = size * 100;
 
         camera.updateProjectionMatrix();
 
-
-
-        //camera.position.copy(center);
-        //  camera.position.x += size / 2.0;
-        //  camera.position.y += size / 5.0;
-        //  camera.position.z += size / 2.0;
-
-
-
-
-
-        //setCamera(camera);
-
-
-
         scene.add(object);
-      
+
 
         // object.traverse( function ( child ) {
         //
@@ -295,18 +233,14 @@ function init(urlAddress) {
         //
         // scene.add( object );
 
-    } );
+    });
 
 
-
-
-
-    window.addEventListener( 'resize', onWindowResize );
+    window.addEventListener('resize', onWindowResize);
 
     // stats
     stats = new Stats();
     //container.appendChild( stats.dom );
-
 
 }
 
@@ -315,23 +249,24 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
-//
+function onTransitionEnd(event) {
+
+    event.target.remove();
+
+}
 
 function animate() {
-
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
     const delta = clock.getDelta();
 
-    if ( mixer ) mixer.update( delta );
+    if (mixer) mixer.update(delta);
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 
     stats.update();
-
 }
-
