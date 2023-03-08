8c7d5c8be5a5c468eaa36c16be03505891499cbf
Max Burnette <mburnet2@illinois.edu>
2023-03-08 11:33:22 -0600

    modifications to indexing

--- app/services/ElasticsearchPlugin.scala
+++ app/services/ElasticsearchPlugin.scala
@@ -527,7 +527,7 @@ class ElasticsearchPlugin(application: Application) extends Plugin {
                 case jv: JsArray => {
                   builder.startArray(clean_k)
                   jv.value.foreach(subv => {
-                    builder.value(subv.toString.replace("\"",""))
+                    builder.value(convertJsObjectToBuilder(builder, subv.asInstanceOf[JsObject]))
                   })
                   builder.endArray()
                 }
@@ -599,10 +599,9 @@ class ElasticsearchPlugin(application: Application) extends Plugin {
           builder.startArray(k.toString.replace(".", "_"))
           v.value.foreach(jv => {
             // Try to interpret numeric value from each String if possible
-            parseDouble(jv.toString) match {
-              case Some(d) => builder.value(d)
-              case None => builder.value(jv)
-            }
+            builder.startObject()
+            convertJsObjectToBuilder(builder, jv.asInstanceOf[JsObject], true)
+            builder.endObject()
           })
           builder.endArray()
         }
