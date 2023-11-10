077ebcadceb571dc4423b35c93e405be868a91a6
Luigi Marini <lmarini@illinois.edu>
2023-11-10 08:55:03 -0600

    Fixed citation.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## 1.22.1 - 2023-10-30
+## 1.22.1 - 2023-11-10
 
 ### Fixed
 - Fixed hearthbeat bug where if the owner was not set in extractors_info.json clowder would throw and error and the
--- version.sh
+++ version.sh
@@ -3,6 +3,7 @@
 VERSION="1.22.1"
 
 sed -i~ "s#^  val version = \".*\"\$#  val version = \"${VERSION}\"#" project/Build.scala
+sed -i~ "s#^version: .*\$#version: \"${VERSION}\"#" citation.cff
 sed -i~ "s#^  version: .*\$#  version: ${VERSION}#" public/swagger.yml
 sed -i~ "s#^release = '.*'\$#release = '${VERSION}'#" doc/src/sphinx/conf.py
 sed -i~ "s/^##.*unreleased.*$/## ${VERSION} - $(date +'%Y-%m-%d')/i" CHANGELOG.md
