1de29c202fb52d6f724b545de8f3208d8fce7179
toddn <tcnichol@illinois.edu>
2024-03-20 15:11:51 -0500

    remove file from folders if we are moving it into the same dataset

--- app/api/Datasets.scala
+++ app/api/Datasets.scala
@@ -716,7 +716,15 @@ class  Datasets @Inject()(
       }
       Logger.debug("----- Adding file to dataset completed")
     } else {
+        val foldersContainingFile = folders.findByFileId(file.id).sortBy(_.name)
         Logger.debug("File was already in dataset.")
+        val foldersContainingFile = folders.findByFileId(file.id).sortBy(_.name)
+        Logger.debug("Remove file from folders in dataset")
+        folders.get(foldersContainingFile).foreach(folder => {
+          if (folder.parentDatasetId == dsId){
+            folders.removeFile(folder.id, fileId)
+          }
+        })
     }
   }
 
